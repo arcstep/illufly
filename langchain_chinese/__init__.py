1a95e4471008dca971ae5b9514c5ba619037a8ed
arcstep <43801@qq.com>
2024-03-03 20:46:52 +0800

    Update dependencies and import statements

--- langchain_chinese/__init__.py
+++ langchain_chinese/__init__.py
@@ -1,9 +1,10 @@
-from langchain_zhipu import (
-    ChatZhipuAI,
-)
-
+from langchain_zhipu import ChatZhipuAI
+from langchain_chinese.document_loaders.base import LocalFilesLoader
+from langchain_chinese.memory.base import WithMemoryBinding
 from langchain_chinese.__version__ import __version__
 
 __all__ = [
     "ChatZhipuAI",
+    "LocalFilesLoader",
+    "WithMemoryBinding",
 ]
