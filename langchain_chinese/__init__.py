a36722d8ebbd4bc59fa110c25da39b7ab8e8a78e
arcstep <43801@qq.com>
2024-03-05 21:19:14 +0800

    Update langchain_chinese package version and add create_qa_chain function

--- langchain_chinese/__init__.py
+++ langchain_chinese/__init__.py
@@ -2,6 +2,7 @@ from langchain_zhipu import ChatZhipuAI
 from langchain_dashscope import ChatDashScope
 
 from langchain_chinese.document_loaders.base import LocalFilesLoader
+from langchain_chinese.retrievers.base import create_qa_chain
 from langchain_chinese.memory import WithMemoryBinding, MemoryManager
 from langchain_chinese.__version__ import __version__
 
@@ -10,4 +11,5 @@ __all__ = [
     "ChatDashScope",
     "LocalFilesLoader",
     "WithMemoryBinding",
+    "create_qa_chain",
 ]
