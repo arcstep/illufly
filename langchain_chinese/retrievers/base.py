47c1f530f156e6b85c0ded9102e370bf95be433d
arcstep <43801@qq.com>
2024-03-05 21:19:02 +0800

    Refactor retrievers/base.py to include a new function create_qa_chain

--- langchain_chinese/retrievers/base.py
+++ langchain_chinese/retrievers/base.py
@@ -1,10 +1,21 @@
-from langchain_core.retrievers import BaseRetriever
-from langchain_core.callbacks import CallbackManagerForRetrieverRun
-from langchain_core.documents import Document
-from typing import List
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 
-class LocalFilesRetriever(BaseRetriever):
-    def _get_relevant_documents(
-        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
-    ) -> List[Document]:
-        return [Document(page_content=query)]
+__DEFAULT_QA_CHAIN_PROMPT = """Answer the question based only on the following context:
+
+{context}
+
+Question: {question}
+"""
+
+def create_qa_chain(llm, retriever, prompt = __DEFAULT_QA_CHAIN_PROMPT):
+    prompt = ChatPromptTemplate.from_template(prompt)
+
+    def format_docs(docs):
+        return "\n\n".join([d.page_content for d in docs])
+
+    return (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+    )
