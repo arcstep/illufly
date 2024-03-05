161f9bbc9d284ab3b01de871fcc19e5b51858a31
arcstep <43801@qq.com>
2024-03-05 12:04:39 +0800

    Add ChatTongyiQW to langchain_chinese package

--- langchain_chinese/__init__.py
+++ langchain_chinese/__init__.py
@@ -1,10 +1,13 @@
 from langchain_zhipu import ChatZhipuAI
+from langchain_dashscope import ChatTongyiQW
+
 from langchain_chinese.document_loaders.base import LocalFilesLoader
 from langchain_chinese.memory import WithMemoryBinding, MemoryManager
 from langchain_chinese.__version__ import __version__
 
 __all__ = [
     "ChatZhipuAI",
+    "ChatTongyiQW",
     "LocalFilesLoader",
     "WithMemoryBinding",
 ]
