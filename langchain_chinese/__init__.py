1ece51069c6bb1be64655b0213a9b6bec0002c3d
arcstep <43801@qq.com>
2024-03-09 22:51:26 +0800

    Update langchain_chinese version to v0.2.21

--- langchain_chinese/__init__.py
+++ langchain_chinese/__init__.py
@@ -3,7 +3,15 @@ from langchain_dashscope import ChatDashScope, DashScopeEmbeddings
 
 from langchain_chinese.document_loaders.base import LocalFilesLoader
 from langchain_chinese.retrievers.base import create_qa_chain
+
+from langchain_chinese.agents.base import (
+    PROMPT_REACT,
+    PROMPT_COT,
+    create_reason_agent,
+)
+
 from langchain_chinese.memory import WithMemoryBinding, MemoryManager
+
 from langchain_chinese.__version__ import __version__
 
 __all__ = [
@@ -16,4 +24,8 @@ __all__ = [
     "LocalFilesLoader",
     "WithMemoryBinding",
     "create_qa_chain",
+    
+    "PROMPT_REACT",
+    "PROMPT_COT",
+    "create_reason_agent",
 ]
