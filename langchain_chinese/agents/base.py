1ec5d49107c401761367b7237155d63ddbc243ce
arcstep <43801@qq.com>
2024-03-09 23:37:54 +0800

    Add typing imports and update code formatting

--- langchain_chinese/agents/base.py
+++ langchain_chinese/agents/base.py
@@ -1,3 +1,4 @@
+from typing import List, Optional, Dict, Any, Union, Callable
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain.agents.agent import AgentOutputParser, AgentAction, AgentFinish
@@ -5,18 +6,17 @@ from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.output_parsers import PydanticOutputParser
 from langchain.prompts import PromptTemplate
 from langchain.tools.render import render_text_description_and_args
-from typing import List, Optional, Dict, Any, Union
 from .prompt import PROMPT_COT, PROMPT_REACT
 
 class Action(BaseModel):
     name: str = Field(
         description="The name of tool or action: FINISH or Other tool names."
         )
-    args: Optional[Dict[str,Any]] = Field(
+    args: Optional[Dict[str, Any]] = Field(
         default=None,
         description="Parameters of tool or action are composed of names and values."
         )
-    
+
 # 解析Action
 _action_output_parser = PydanticOutputParser(pydantic_object=Action)
 _action_parser_format = _action_output_parser.get_format_instructions()
@@ -26,26 +26,22 @@ class ReasonOutputParser(AgentOutputParser):
     """
 
     def parse(self, text: str) -> Union[AgentAction, AgentFinish]:
-        action = _action_output_parser.invoke(text)
-        name = action.name
-        args = action.args if text is not None else "No Args"
-        log = text if text is not None else ""
-        print("-"*80)
-        # print(args)
-        print(name)
-        # print(log)
-        
-        if(name == "FINISH"):
+        action: Action = _action_output_parser.invoke(text)
+        name: Optional[str] = action.name
+        args: Optional[Dict[str, Any]] = action.args if text is not None else "No Args"
+        log: str = text if text is not None else ""
+
+        if name == "FINISH":
             return AgentFinish(args, log)
-        elif(name is not None):
+        elif name is not None:
             return AgentAction(name, args, log)
 
     @property
     def _type(self) -> str:
         return "Chain-of-Thought"
-    
-def _prompt_creator(prompt):
-    def creator(tools):
+
+def _prompt_creator(prompt: str) -> Callable[[List[str]], str]:
+    def creator(tools: List[str]) -> str:
         tools_format = render_text_description_and_args(tools)
 
         template = PromptTemplate.from_template(prompt)
@@ -53,12 +49,15 @@ def _prompt_creator(prompt):
             tools=tools_format,
             action_format_instructions=_action_parser_format,
         )
+
     return creator
 
-def create_reason_agent(llm, prompt=None, tools=[]):
+# 基于 ReAct / CoT 的智能体
+def create_reason_agent(llm: Any, prompt: Optional[str] = None, tools: List[str] = []) -> Any:
     prompt_creator = _prompt_creator(PROMPT_COT)
     if prompt is not None:
         prompt_creator = _prompt_creator(prompt)
+
     agent = (
         RunnablePassthrough.assign(
             agent_scratchpad=lambda x: format_log_to_str(x["intermediate_steps"])
