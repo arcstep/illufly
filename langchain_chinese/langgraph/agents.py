3a2b18713df67f4b21af10eea32a035cf5017487
arcstep <43801@qq.com>
2024-03-11 21:10:55 +0800

    Refactor language chain agents and prompt

--- langchain_chinese/langgraph/agents.py
+++ langchain_chinese/langgraph/agents.py
@@ -14,8 +14,10 @@ from langchain_core.runnables import RunnableLambda
 from langchain_core.tools import BaseTool
 from langchain_core.utils.function_calling import convert_to_openai_tool
 
-from langgraph.graph import END, StateGraph
+from langgraph.graph import END, MessageGraph
 from langgraph.prebuilt.tool_executor import ToolExecutor, ToolInvocation
+
+from langchain_core.output_parsers import BaseOutputParser
 
 def create_tool_calling_executor(
 	model: LanguageModelLike, tools: Union[ToolExecutor, Sequence[BaseTool]]
@@ -26,39 +28,19 @@ def create_tool_calling_executor(
 	else:
 		tool_executor = ToolExecutor(tools)
 		tool_classes = tools
-	model = model.bind(tools=[convert_to_openai_tool(t) for t in tool_classes])
-	
-	class AgentState(TypedDict):
-		messages: Annotated[Sequence[BaseMessage], operator.add]
 
-	def should_continue(state: AgentState):
-		messages = state["messages"]
+	reasoning_chain = (
+		model.bind(tools=[convert_to_openai_tool(t) for t in tool_classes])
+	)
+	
+	def should_continue(messages):
 		last_message = messages[-1]
 		if "tool_calls" not in last_message.additional_kwargs:
 			return "end"
 		else:
 			return "continue"
 
-	def call_model(state: AgentState):
-		messages = state["messages"]
-		message = AIMessage(content="")
-		for chunk in model.stream(messages):
-			message.content += chunk.content
-			if chunk.additional_kwargs:
-				message.additional_kwargs = chunk.additional_kwargs
-		return {"messages": [message]}
-
-	async def acall_model(state: AgentState):
-		messages = state["messages"]
-		message = AIMessage(content="")
-		async for chunk in model.astream(messages):
-			message.content += chunk.content
-			if chunk.additional_kwargs:
-				message.additional_kwargs = chunk.additional_kwargs
-		return {"messages": [message]}
-
-	def _get_actions(state: AgentState):
-		messages = state["messages"]
+	def _get_actions(messages):
 		last_message = messages[-1]
 		return (
 			[
@@ -74,28 +56,28 @@ def create_tool_calling_executor(
 			],
 		)
 
-	def call_tool(state: AgentState):
-		actions, ids = _get_actions(state)
+	def call_tool(messages):
+		actions, ids = _get_actions(messages)
 		responses = tool_executor.batch(actions)
 		assert len(actions) == len(responses), "The number of actions and responses should be the same"
 		tool_messages = [
 			ToolMessage(content=str(response), tool_call_id=id)
 			for response, id in zip(responses, ids)
 		]
-		return {"messages": tool_messages}
+		return tool_messages
 
-	async def acall_tool(state: AgentState):
-		actions, ids = _get_actions(state)
+	async def acall_tool(messages):
+		actions, ids = _get_actions(messages)
 		responses = await tool_executor.abatch(actions)
 		tool_messages = [
 			ToolMessage(content=str(response), tool_call_id=id)
 			for response, id in zip(responses, ids)
 		]
-		return {"messages": tool_messages}
+		return tool_messages
 
-	workflow = StateGraph(AgentState)
+	workflow = MessageGraph()
 
-	workflow.add_node("agent", RunnableLambda(call_model, acall_model))
+	workflow.add_node("agent", reasoning_chain)
 	workflow.add_node("action", RunnableLambda(call_tool, acall_tool))
 
 	workflow.set_entry_point("agent")
