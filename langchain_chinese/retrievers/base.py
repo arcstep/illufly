2caff81843a37b3f66656089fe8b30cb97be2dc4
arcstep <43801@qq.com>
2024-03-18 10:32:24 +0800

    Update langchain_chinese/retrievers/base.py

--- langchain_chinese/retrievers/base.py
+++ langchain_chinese/retrievers/base.py
@@ -1,14 +1,18 @@
-from typing import List, Callable, Any, Optional, Type
+from typing import List, Callable, Any, Optional, Type, Union
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough, Runnable
+from langchain_core.runnables import RunnablePassthrough, Runnable, RunnableAssign
 from langchain_core.output_parsers import StrOutputParser
 from langchain.agents import tool
 from langchain.tools import BaseTool
 from langchain.pydantic_v1 import BaseModel, Field
 from langchain.callbacks.manager import CallbackManagerForToolRun
+from langchain_core.messages import BaseMessage
 from functools import wraps
+from operator import itemgetter
 
-__DEFAULT_QA_CHAIN_PROMPT = """
+ASK_DOCUMENT_TOOL_NAME = "ask_document"
+
+DEFAULT_QA_CHAIN_PROMPT = """
 你要严格依据如下资料回答问题，你的回答不能与其冲突，更不要编造。
 请始终使用中文回答。
 
@@ -17,17 +21,33 @@ __DEFAULT_QA_CHAIN_PROMPT = """
 问题: {question}
 """
 
-def create_qa_chain(llm: Runnable, retriever: Callable, prompt: str = __DEFAULT_QA_CHAIN_PROMPT) -> Callable:
+def create_qa_chain(llm: Runnable, retriever: Callable, prompt: str = DEFAULT_QA_CHAIN_PROMPT) -> Callable:
+    """
+    使用 create_qa_chain 构建的LCEL链时，参数应当是一个消息列表。
+    
+    例如：    
+    chain = creat_qa_chain(llm, rectriever)
+    chain.invoke(["langchain_chinese是啥？"])
+    """
+
     prompt = ChatPromptTemplate.from_template(prompt)
 
     def format_docs(docs: List[str]) -> str:
         return "\n\n".join([d.page_content for d in docs])
-
+    
+    def convert_message_to_str(message: Union[BaseMessage, str]) -> str:
+        if isinstance(message, BaseMessage):
+            return message.content
+        else:
+            return message
+    
     return (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        {
+            "context": (lambda x: convert_message_to_str(x[0])) | retriever | format_docs,
+            "question": lambda x: convert_message_to_str(x[0]) ,
+        }
         | prompt
         | llm
-        | StrOutputParser()
     )
 
 def make_safe_tool(func: Callable) -> Callable:
@@ -41,11 +61,11 @@ def make_safe_tool(func: Callable) -> Callable:
     return wrapper
 
 class SearchInput(BaseModel):
-    query: str = Field(title = "提问内容", description="用户问题的文字描述，必须是字符串")
+    query: str = Field(title="提问内容", description="用户问题的文字描述，必须是字符串")
 
 class AskDocumentTool(BaseTool):
-    name = "ask_document"
-    description = """根据资料库回答问题。考虑上下文信息，确保问题对相关概念的定义表述完整。
+    name: str = ASK_DOCUMENT_TOOL_NAME
+    description: str = """根据资料库回答问题。考虑上下文信息，确保问题对相关概念的定义表述完整。
     args:
     - query 类型是str, 用户问题的文字描述
     """
@@ -58,5 +78,5 @@ class AskDocumentTool(BaseTool):
         """Use the tool."""
         return self.qa_chain.invoke(query)
 
-def create_qa_toolkits(qa_chain: Runnable) -> List[AskDocumentTool]:
-    return [AskDocumentTool(qa_chain=qa_chain)]
+def create_qa_toolkits(qa_chain: Runnable, name: str = None, description: str = None) -> List[AskDocumentTool]:
+    return [AskDocumentTool(qa_chain, name, description)]
