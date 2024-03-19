2d5ceeb026ae25023e9a95907b09658527b11f7e
arcstep <43801@qq.com>
2024-03-20 01:29:49 +0800

    Update version to v0.2.25 and remove unused imports

--- langchain_chinese/__init__.py
+++ langchain_chinese/__init__.py
@@ -1,6 +1,3 @@
-from langchain_zhipu import ChatZhipuAI, ZhipuAIEmbeddings
-from langchain_dashscope import ChatDashScope, DashScopeEmbeddings
-
 from langchain_chinese.document_loaders.base import LocalFilesLoader
 from langchain_chinese.retrievers.base import (
     AskDocumentTool,
@@ -19,12 +16,6 @@ from langchain_chinese.memory import WithMemoryBinding, MemoryManager
 from langchain_chinese.__version__ import __version__
 
 __all__ = [
-    "ChatZhipuAI",
-    "ZhipuAIEmbeddings",
-
-    "ChatDashScope",
-    "DashScopeEmbeddings",
-
     "LocalFilesLoader",
     "WithMemoryBinding",
     "AskDocumentTool",
