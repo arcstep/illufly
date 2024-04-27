c8adda9d2f37659f0931ea68eaecd9b1cebe95b6
arcstep <43801@qq.com>
2024-04-27 17:02:53 +0800

    Refactor langchain_chinese/document_loaders/base.py to add new loaders for QA knowledge

--- langchain_chinese/document_loaders/base.py
+++ langchain_chinese/document_loaders/base.py
@@ -1,19 +1,25 @@
 from typing import Iterator, List, Union
 from langchain_core.documents import Document
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders.base import BaseLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import UnstructuredMarkdownLoader
 
 import os
 import re
 
 class BaseQALoader(BaseLoader):
     """
-    Load QA knowledge from local files as Documents.
+    从本地文件中检索知识文档。
+    
+    包含的过滤规则有：
+    - 从 LANGCHAIN_CHINESE_DOCUMENTS_FOLDER 变量指定的路径列举文件
+    - 按照 question_filter 过滤文档路径，但不要过滤 LANGCHAIN_CHINESE_DOCUMENTS_FOLDER 指定的部份
+    - 按照 extensions 过滤指定扩展名的文件
     """
     
     # support types
-    extensions: List[str] = ["docx", "pdf"]
+    extensions: List[str] = ["docx", "pdf", "md"]
     
     # root document folder storage
     _documents_folder: str = "./documents"
@@ -29,8 +35,8 @@ class BaseQALoader(BaseLoader):
     # question file filter
     question_filter: str = ".*"
     
-    # knowledge file filter
-    knowledge_filter: str = ".*"
+    # answer file filter
+    answer_filter: str = ".*"
     
     # only include folder or files
     _includes: List[str] = []
@@ -71,47 +77,54 @@ class BaseQALoader(BaseLoader):
         else:
             self.documents_folder = documents_folder
 
-        for key in ["extensions", "includes", "excludes"]:
+        for key in ["extensions", "includes", "excludes", "question_filter", "answer_filter"]:
             if(kwargs.get(key) is not None):
                 setattr(self, key, kwargs.get(key))
 
     def get_files(self) -> list[str]:
-        """
-        返回本地知识库中的文档，包含如下规则：
-        - 从 LANGCHAIN_CHINESE_DOCUMENTS_FOLDER 变量指定的路径列举文件
-        - 按照 question_filter 过滤文档路径，但不要过滤 LANGCHAIN_CHINESE_DOCUMENTS_FOLDER 指定的部份
-        - 按照 extensions 过滤指定扩展名的文件
-        """
+        """List All Files with Extension"""
         files = []
 
         # get all files defaults
         folders = self.documents_folder
+        print(f"question_filter: {self.question_filter}")
+        print(f"extensions: {self.extensions}")
         for dirpath, dirnames, filenames in os.walk(folders):
+            print("-"*20)
+            print(dirpath, filenames)
             for filename in filenames:
                 relpath = os.path.relpath(os.path.join(dirpath, filename), folders)
                 if(self.extensions == []):
-                    if re.match(self.question_filter, relpath):
+                    if re.search(self.question_filter, relpath):
                         files.append(os.path.join(dirpath, filename))
                 else:
-                    if get_file_extension(filename) in self.extensions and re.match(self.question_filter, relpath):
+                    if get_file_extension(filename) in self.extensions and re.search(self.question_filter, relpath):
                         files.append(os.path.join(dirpath, filename))
+                print(relpath)
+                print(files)
 
         # filter files with includes
         if(self.includes != []):
+            print(f"include folders: {self.excludes}")
             files = [f for f in files if any(f.startswith(include) for include in self.includes)]
+            print(files)
 
         # filter files with excludes
         if(self.excludes != []):
-            print(f"excludes: {self.excludes}")
+            print(f"exclude folders: {self.excludes}")
             files = [f for f in files if not any(f.startswith(exclude) for exclude in self.excludes)]
-        
+            print(files)
+
         return files
 
     def load_docs(self, filename: str) -> List[Document]:
         """Load file as Documents by FileLoadFactory"""
         file_loader = FileLoadFactory.get_loader(filename)
-        pages = file_loader.load()
-        return pages
+        if file_loader:
+            pages = file_loader.load()
+            return pages
+        else:
+            return []
 
     def lazy_load(self) -> Iterator[Document]:
         """Load files as Documents by FileLoadFactory"""
@@ -129,11 +142,19 @@ class FileLoadFactory:
         filename = filename.strip()
         ext = get_file_extension(filename)
         if ext == "pdf":
-            return PyPDFLoader(filename)
+            loader = PyPDFLoader(filename)
+            return loader
         elif ext == "docx":
-            return Docx2txtLoader(filename)
+            loader = Docx2txtLoader(filename)
+            return loader
+        elif ext == "md":
+            loader = UnstructuredMarkdownLoader(
+                filename, mode="elements", strategy="fast",
+            )
+            return loader
         else:
-            raise NotImplementedError(f"File extension {ext} not supported now.")
+            print(f"WARNING: Loaded File extension {ext} not supported now.")
+            return None
 
 def get_file_extension(filename: str) -> str:
     """Get File Extension"""
