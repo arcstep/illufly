896ceb829d6f218f009c9e1348b3a0875e421dfa
arcstep <43801@qq.com>
2024-05-16 21:43:57 +0800

    Refactor langchain_chinese/writing/ai.py to improve AI initialization and memory management

--- langchain_chinese/writing/ai.py
+++ langchain_chinese/writing/ai.py
@@ -1,9 +1,8 @@
 from typing import Any, Dict, Iterator, List, Optional, Union, Tuple
 from langchain_core.runnables import Runnable
-from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
-from ..memory.history import LocalFileMessageHistory, create_session_id
-from ..memory.memory_manager import MemoryManager
-from ..memory.base import WithMemoryBinding
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ConversationBufferWindowMemory
 from .prompts.task_prompt import *
 import os
 
@@ -12,7 +11,7 @@ class BaseAI():
     向AI提问。
     """
     
-    def __init__(self, llm: Runnable = None, memory: Optional[MemoryManager] = None):
+    def __init__(self, llm: Runnable = None, streaming: bool = True):
         if llm == None:
             if os.environ.get("ZHIPUAI_API_KEY"):
                 from langchain_zhipu import ChatZhipuAI
@@ -25,7 +24,9 @@ class BaseAI():
         else:
             self.llm = llm
 
-        self.memory = memory
+        self.memory = ConversationBufferWindowMemory(k=20, return_messages=True)
+        self.streaming = streaming
+
         self.retry_max: int = 5
     
     def prompt_default(self):
@@ -33,7 +34,7 @@ class BaseAI():
 
         prompt = ChatPromptTemplate.from_messages([
             ("system", default_prompt),
-            ("ai", "好的。"),
+            ("ai", "OK"),
             MessagesPlaceholder(variable_name="history"),
             ("human", "{{task}}"),
         ], template_format="jinja2")
@@ -48,7 +49,7 @@ class BaseAI():
 
         prompt = ChatPromptTemplate.from_messages([
             ("system", main_prompt),
-            ("ai", "好的，我会尽最大努力。"),
+            ("ai", "OK"),
             MessagesPlaceholder(variable_name="history"),
             ("human", "{{task}}"),
         ], template_format="jinja2").partial(
@@ -66,13 +67,15 @@ class BaseAI():
         self,
         title: str,
         content_type: str = "paragraph",
-        words_per_step: int = 500,
+        words_limit: int = 500,
         words_advice: int = 500,
         howto: str = None,
         outline_exist: List[Any] = None,
     ):
         main_prompt = MAIN_PROMPT
         auto_prompt = _AUTO_OUTLINE_OR_PARAGRAPH_PROMPT
+        json_instruction = _JSON_INSTRUCTION 
+
         if content_type == "outline":
             task_prompt   = _OUTLINE_TASK
             output_format = _OUTLINE_FORMAT
@@ -82,21 +85,20 @@ class BaseAI():
 
         prompt = ChatPromptTemplate.from_messages([
             ("system", main_prompt),
-            ("ai", "你对我的写作有什么要求？"),
+            ("ai", "有什么具体要求？"),
             ("human", auto_prompt),
-            ("ai", "好的，我会尽最大努力。"),
+            ("ai", "OK"),
             MessagesPlaceholder(variable_name="history"),
             ("human", "{{task}}")
         ], template_format="jinja2").partial(
             # 字数限制
-            words_limit=self.words_per_step,
-            words_advice=self.todo_content.words_advice,
-            # 写作提纲
-            title=self.todo_content.title,
+            words_limit=words_limit,
+            words_advice=words_advice,
+            # 写作要求
+            title=title,
             outline_exist=outline_exist,
-            # 任务指南
             task_instruction=task_prompt,
-            howto=self.todo_content.howto,
+            howto=howto,
             # 输出格式要求
             output_format=output_format,
             # JSON严格控制
@@ -110,48 +112,47 @@ class BaseAI():
     
     def get_chain(self, prompt = None):
         # 构造链
-        chain = (prompt or self.prompt_default()) | self.llm
-        # print(prompt.format(task="<<DEMO_TASK>>", history=[]))
+        if prompt == None:
+            chain = self.prompt_default() | self.llm
+        else:
+            chain = prompt | self.llm
 
-        # 记忆绑定管理
-        withMemoryChain = WithMemoryBinding(
-            chain,
-            memory,
-            input_messages_key="task",
-            history_messages_key="history",
-        )
+        return chain
 
-        return withMemoryChain
-
-    def ask_ai(self, chain: Runnable, session_id = "default", return_json = True, **kwargs):
+    def ask_ai(self, task, chain: Runnable = None, return_json = True):
         """AI推理"""
+
+        chain = chain or self.get_chain()
 
         counter = 0
         while(counter < self.retry_max):
             counter += 1
             try:
-                config = {"configurable": {"session_id": session_id}}
-                kwargs["config"] = config
                 text = ""
                 if self.streaming:
-                    for resp in chain.stream(**kwargs):
+                    for resp in chain.stream({"task": task, "history": self.memory.buffer}):
                         print(resp.content, end="", flush=True)
                         text += resp.content
                     print()
                 else:
-                    resp = chain.invoke(**kwargs)
+                    resp = chain.invoke({"task": task, "history": self.memory.buffer})
                     print("resp:", resp.content)
                     text = resp.content
 
+                # 存储到记忆管理中
+                self.memory.chat_memory.add_user_message(task)
+                self.memory.chat_memory.add_ai_message(text)
+
                 if return_json:
-                    return text
-                else:
+                    print("要求输出JSON ...")
                     json = JsonOutputParser().invoke(input=text)
                     if json:
                         return json
                     else:
                         raise BaseException("JSON为空")
+                else:
+                    return text
             except Exception as e:
-                print(f"推理错误: {e}")
+                print(f"推理错误: ", e)
 
         raise Exception(f"AI返回结果无法正确解析，已经超过 {self.retry_max} 次，可能需要调整提示语模板！！")
