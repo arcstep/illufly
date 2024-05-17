18e34cf4e97169f4f4d5920e292c253e777e7b7f
arcstep <43801@qq.com>
2024-05-17 23:02:41 +0800

    Update version to 0.1.2 in pyproject.toml and __version__.py

--- textlong/__init__.py
+++ textlong/__init__.py
@@ -1,35 +1,35 @@
-from langchain_chinese.document_loaders.base import (
+from textlong.document_loaders.base import (
     LocalFilesLoader,
     LocalFilesQALoader,
 )
 
-from langchain_chinese.retrievers.base import (
+from textlong.retrievers.base import (
     AskDocumentTool,
     create_qa_chain,
     create_qa_toolkits,
 )
 
-from langchain_chinese.agents.base import (
+from textlong.agents.base import (
     PROMPT_REACT,
     PROMPT_COT,
     create_reason_agent,
 )
 
-from langchain_chinese.base import BaseProject, WritingChain
-from langchain_chinese.serialize import ContentSerialize
-from langchain_chinese.command import BaseCommand
-from langchain_chinese.state import ContentState
-from langchain_chinese.node import ContentNode
-from langchain_chinese.tree import ContentTree
-from langchain_chinese.task import WritingTask
-from langchain_chinese.ai import BaseAI
+from textlong.base import BaseProject, WritingChain
+from textlong.serialize import ContentSerialize
+from textlong.command import BaseCommand
+from textlong.state import ContentState
+from textlong.node import ContentNode
+from textlong.tree import ContentTree
+from textlong.task import WritingTask
+from textlong.ai import BaseAI
 
-from langchain_chinese.memory import WithMemoryBinding, MemoryManager
+from textlong.memory import WithMemoryBinding, MemoryManager
 
-from langchain_chinese.memory.history import (
+from textlong.memory.history import (
     LocalFileMessageHistory,
     create_session_id,
     parse_session_id,
 )
 
-from langchain_chinese.__version__ import __version__
+from textlong.__version__ import __version__
