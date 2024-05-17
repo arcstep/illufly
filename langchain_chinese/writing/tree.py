d08d476b2216eca2ab1c66d475fc975c4025c89b
arcstep <43801@qq.com>
2024-05-17 11:39:40 +0800

    Refactor langchain_chinese/writing/tree.py to improve code organization and maintainability

--- langchain_chinese/writing/tree.py
+++ langchain_chinese/writing/tree.py
@@ -1,6 +1,4 @@
 from typing import Any, Dict, Iterator, List, Optional, Union
-from langchain.memory import ConversationBufferWindowMemory
-from ..memory.memory_manager import MemoryManager
 from .node import ContentNode
 from .command import BaseCommand
 
@@ -14,77 +12,51 @@ class ContentTree(BaseCommand):
     ):
         super().__init__(**kwargs)
 
-        self.todo_node = TreeContent() if todo_node == None else todo_node
+        self.todo_node = ContentNode() if todo_node == None else todo_node
 
     @property
     def root(self):
         return self.todo_node.root
-    
+
+    # inherit
     @property
-    def focus(self):
-        return self.todo_node.id
+    def default_command(self) -> str:
+        return self.todo_node.default_command
 
     # inherit
+    @property
     def commands(self) -> List[str]:
-        return []
+        return ["move", "todo", "draft", "all"] + self.todo_node.commands
 
     # inherit
-    def parse(self, user_said: str) -> tuple:
-        if user_said is None:
-            return {"id": None, "command": None, "args": None}
+    def call(self, command, args, **kwargs):
+        if command == 'move' and args:
+            # 转移到新的指定对象
+            obj = self.root.get_item_by_id(resp['id'])
+            self.todo_node = obj or self.todo_node
+            return obj != None
 
-        pattern = r'^\s*(?:<([\w.-]+)>)?\s*(' + '|'.join(self.__class__.commands()) + r')?\s*(.*)$'
-        match = re.match(pattern, user_said, re.IGNORECASE)
-
-        if match:
-            content_id, command, args = match.groups()
-
-        return {"id": content_id, "command": command, "args": args}
-
-    # inherit
-    def invoke(self, user_said: str) -> Any:
-        resp = self.parser(user_said)
-
-        if resp and resp['command'] in self.commands():
-            resp['reply'] = self.call(**resp)
-        else:
-            if resp['id'] == None:
-                obj = self.todo_content
+        elif command == 'todo':
+            # 转移到新的待完成对象
+            if self.todo_node.is_complete:
+                obj = self.root.find_not_complete_node()
+                if obj:
+                    self.todo_node = obj
             else:
-                obj = self.root_content.get_item_by_id(resp['id'])
-            if obj and resp['command'] in obj.commands:
-                resp['reply'] = obj.call(**resp)
+                obj = self.todo_node
+            return obj.id if obj else None
 
-        return resp
-
-    # inherit
-    def call(self, command, **kwargs):
-        resp = self.parser(user_said)
-        resp['reply'] = None
-
-        return resp
-
-    def move_focus(self, id: str) -> str:
-        """
-        移动到指定节点。
-        """
-        target = self.root.get_item_by_id(id)
-        if target:
-            self.todo_node = target
+        elif command == 'draft':
+            # 转移到新的草稿对象
+            if not self.todo_node.is_draft:
+                obj = self.root.find_draft_node()
+                if obj:
+                    self.todo_node = obj
+            else:
+                obj = self.todo_node
+            return obj.id if obj else None
+        elif command == "all":
+            return self.root.all_content
         else:
-            # 在对象树中无法找到内容ID
-            raise BaseException("Invalid content ID:", id)
-
-        return self.todo_node.id
-
-    def move_focus_auto(self) -> str:
-        """
-        从root开始遍历所有未完成的节点。
-        """
-        next_todo = self.root.not_complete_child
-        if next_todo:
-            self.move_focus(next_todo.id)
-            return True
-        else:
-            return False
+            return self.todo_node.call(command, args, **kwargs)
 
