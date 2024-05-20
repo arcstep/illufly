d9de5cb24c4070ce75ec6a2ae8227485abc36583
arcstep <43801@qq.com>
2024-05-20 10:18:25 +0800

    Add LocalFileMessageHistory class to store chat message history in a local file

--- textlong/config.py
+++ textlong/config.py
@@ -4,6 +4,7 @@ _NODES_FOLDER_NAME = "__NODES__"
 _TEMPLATES_FOLDER_NAME = "__TEMPLATES__"
 _CONTENTS_FOLDER_NAME = "__CONTENTS__"
 _PROMPTS_FOLDER_NAME = "__PROMPTS__"
+_HISTORY_FOLDER_NAME = "__HISTORY__"
 
 def get_textlong_folder():
     """从环境变量中获得项目的存储目录"""
