a226a99ddc56cadfe31d1f74aa6050972194fe37
arcstep <43801@qq.com>
2024-05-30 02:02:51 +0800

    add get_documents_range to improve structure and readability

--- textlong/md/documents.py
+++ textlong/md/documents.py
@@ -1,23 +1,26 @@
 import re
 from typing import List, Union
 from langchain_core.documents import Document
+import copy
 
 class IntelliDocuments():
     def __init__(self, doc_str: str=None, start_id="1", llm=None):
         self.llm = llm
         self.documents = []
-        self.import_markdown(doc_str, start_id)
 
-    def import_markdown(self, doc_str: str=None, start_id="1"):
+        if doc_str != None:
+            documents = self.parse_markdown(doc_str)
+            self.documents.extend(documents)
+            # self.insert_documents(documents, start_id)
+
+    def import_markdown(self, doc_str: str=None, to_id="99999"):
         """
         导入Markdown文档。
         """
         if doc_str != None:
-            self.documents += self.parse_markdown(doc_str)
+            document = self.parse_markdown(doc_str)
+            self.replace_documents(document, to_id)
             
-            if self.documents:
-                self.build_index(start_id)
-        
         return self.documents
 
     @classmethod
@@ -26,6 +29,9 @@ class IntelliDocuments():
         解析 Markdown 文件。
         给定的 Markdown 中的第一个标题应当是最大的标题。
         """
+
+        if not isinstance(doc_str, str):
+            raise ValueError("parse_markdown: doc_str ONLY accept str !")
 
         # Step 1: Replace the content in ``` <<content>> ``` with a special marker
         pattern = r'```.*?```'
@@ -38,7 +44,7 @@ class IntelliDocuments():
         pattern = r'(#{1,3})\s*(.*?)\n|((?:(?<=\n)|^)(?!\s*#).*?(?=\n\s*#|$))'
         matches = re.findall(pattern, doc_str, re.DOTALL)
         documents = []
-        max_heading = 999
+        max_heading = 9999
         for match in matches:
             if match[0]:  # This is a title
                 type_ = 'H' + str(len(match[0]))
@@ -58,12 +64,13 @@ class IntelliDocuments():
 
         # Step 4: Check the first document's type
         # print('H' + str(max_heading))
-        if documents and max_heading != 999 and (documents[0].metadata['type'] != f'H{max_heading}'):
-            documents.insert(0, Document(page_content='<<TITLE>>', metadata={'type': f'H{max_heading}'}))
+        # if documents and (documents[0].metadata['type'] != f'H{max_heading}'):
+        #     documents.insert(0, Document(page_content='<<TITLE>>', metadata={'type': f'H{max_heading}'}))
 
         return documents
 
-    def build_index(self, start_id: str="1.1"):
+    @classmethod
+    def build_index(self, documents: List[Document], start_id: str="1"):
         """构建层级编号"""
 
         indices = {f'H{i}': 0 for i in range(1, 9)}  # Initialize all indices to 0
@@ -79,91 +86,86 @@ class IntelliDocuments():
         tail = ""
         middle = ""
 
-        for doc in self.documents:
+        for doc in documents:
             type_ = doc.metadata.get('type')
             if type_ in indices:
                 if type_.startswith('H'):
-                    # Increment the current level index
                     indices[type_] += 1
-                    # Save the current level index
                     last_index = indices[type_]
-                    # Reset the indices of the lower levels
                     for lower_type in [f'H{i}' for i in range(int(type_[1:]) + 1, 9)] + ['paragraph']:
                         indices[lower_type] = 0
                     last_heading = type_
-                    # Update tail_ids, middle, tail and doc.metadata['id']
                     tail_ids = [str(indices[f'H{i}']) if i < int(last_heading[1]) else str(last_index) for i in range(1, int(last_heading[1]) + 1)]
                     middle = f"{int(tail_ids[0]) - 1 + start_int}"
                     tail = ".".join(tail_ids[1:])
                     # print(f'{prefix or "<EMPTY>"}-{middle}({tail_ids[0]}/{start_int})-{tail}')
                     doc.metadata['id'] = ".".join([e for e in [prefix, middle, tail] if e != ""])
                 elif type_ == 'paragraph' and last_heading:
-                    # Set the paragraph index to 0
                     indices['paragraph'] = 0
-                    # Update tail_ids and doc.metadata['id']
                     tail_ids.append(str(indices['paragraph']))
                     tail = ".".join(tail_ids[1:])
                     doc.metadata['id'] = ".".join([e for e in [prefix, middle, tail] if e != ""])
                     # print(f"{prefix or 'EMPTY'}-{middle}-{tail_ids}")
                     tail_ids.pop()
+                else:
+                    doc.metadata['id'] = '0'
 
-    def get_documents(self, id: Union[str, List[str]]="1", node_type: Union[str, List[str]]=None):
-        """获得文档子树"""
+        return documents
 
-        if isinstance(id, str):
-            id = [id]
-        if isinstance(node_type, str):
-            node_type = [node_type]
-        if node_type == None:
-            node_type = ['H', 'par'] # [H1,H2,...H6, paragraph]
-
-        return [
-            doc
-            for doc in self.documents
-            if doc.metadata
-            and 'id' in doc.metadata
-            and any(doc.metadata['id'].startswith(i) for i in id)
-            and 'type' in doc.metadata
-            and any(doc.metadata['type'].startswith(i) for i in node_type)
-        ]
-
-    def replace_documents(self, node_id: str, documents: List[Document], sorting=True):
-        """在指定位置替换文档子树"""
-
-        self.remove_documents(node_id, sorting=False)
-        self.insert_documents(node_id, documents, sorting=False)
-        if sorting:
-            self.documents.sort(key=lambda doc: doc.metadata['id'])
-
-        return self.documents
-
-    def insert_documents(self, node_id: str, documents: List[Document], sorting=True):
-        """添加子树，并按照 Document.metadata['id'] 重新排序"""
-
-        if node_id is None:
-            self.documents.extend(documents)
-        else:
+    def get_documents_range(self, title: str=None):
+        start_index = None
+        end_index = None
+        if title != None:
+            last_header_doc = None
             for i, doc in enumerate(self.documents):
-                if doc.metadata['id'] == node_id:
-                    for j, new_doc in enumerate(documents):
-                        self.documents.insert(i + j, new_doc)
+                print(i, doc)
+                with_header = doc.metadata['type'].startswith("H")
+                if doc.page_content.startswith(title):
+                    start_index = i
+                    end_index = i
+                    if with_header:
+                        last_header_doc = doc
+                    continue
+                if last_header_doc:
+                    if with_header and doc.metadata['type'] > last_header_doc.metadata['type']:
+                        continue
+                    elif not with_header:
+                        continue
+                    # 标题已入栈且未找到更深标题或段落时退出
+                    end_index = i
                     break
+        return (start_index, end_index if start_index != end_index else None)
 
-        if sorting:
-            self.documents.sort(key=lambda doc: doc.metadata['id'])
-
+    def get_documents(self, title: str=None, node_type: Union[str, List[str]]=None):
+        """获得文档子树"""
+        start_index, end_index = self.get_documents_range(title)
+        return self.documents[start_index:end_index]
+    
+    def replace_documents(self, new_docs: List[Document], title: str=None):
+        """在指定位置替换文档子树"""
+        
+        start_index, end_index = self.get_documents_range(title)
+        if start_index == None or end_index == None:
+            self.documents = self.documents + new_docs
+        else:
+            self.documents = self.documents[:start_index] + new_docs + self.documents[end_index:None]
         return self.documents
 
-    def remove_documents(self, node_id: str, sorting = True):
+    def insert_documents(self, new_docs: List[Document], title: str=None):
+        """插入文档到指定位置"""
+
+        start_index, end_index = self.get_documents_range(title)
+        if start_index == None or end_index == None:
+            self.documents = self.documents + new_docs
+        else:
+            self.documents = self.documents[:start_index] + new_docs + self.documents[start_index:]
+        return self.documents
+
+    def remove_documents(self, title: str):
         """删除指定的子树"""
-
-        indices = [i for i, doc in enumerate(self.documents) if doc.metadata['id'].startswith(node_id)]
-        if indices:
-            for index in reversed(indices):
-                del self.documents[index]
-            if sorting:
-                self.documents.sort(key=lambda doc: doc.metadata['id'])
-
+        start_index, end_index = self.get_documents_range(title)
+        if start_index and end_index:
+            self.documents = self.documents[:start_index] + self.documents[end_index:]
         return self.documents
 
     def get_markdown_header(self, document: Document=None, with_number: bool=True):
@@ -197,35 +199,38 @@ class IntelliDocuments():
         
         return header
 
-    def get_markdown(self, id: Union[str, List[str]]=None, node_type: Union[str, List[str]]=None, with_number: bool=False):
+    def get_markdown(self, title: Union[str, List[str]]=[], node_type: Union[str, List[str]]=None, with_number: bool=False):
         """
         导出 Markdown 文本。
         """
 
-        docs = self.get_documents(node_type=node_type) if id == None else self.get_documents(id, node_type=node_type)
+        docs = self.get_documents(node_type=node_type) if title == None else self.get_documents(title, node_type=node_type)
         md = ""
         
         for doc in docs:
-            md += self._get_text(doc, with_number)
+            md += self.get_node_text(doc, with_number)
         
         return md
 
-    def get_todo_ids(self, start_id="1"):
+    def get_trunk_and_leaf_nodes(self, start_id=[]):
         """获得提纲任务"""
-        tasks = []
+        trunk_nodes = []
+        leaf_nodes = []
         docs = self.get_documents(id=start_id, node_type="H")
         len_docs = len(docs)
         for i, doc in enumerate(docs):
             if doc.metadata and doc.metadata['id']:
                 # 检查是否有子标题
                 if i + 1 < len_docs and not docs[i + 1].metadata['id'].startswith(doc.metadata['id'] + "."):
-                    tasks.append(doc.metadata['id'])
+                    leaf_nodes.append(doc)
                 elif i + 1 == len_docs:
-                    tasks.append(doc.metadata['id'])
+                    leaf_nodes.append(doc)
+                else:
+                    trunk_nodes.append(doc)
 
-        return tasks
+        return trunk_nodes, leaf_nodes
     
-    def _get_text(self, document: Document=None, with_number: bool=False):
+    def get_node_text(self, document: Document=None, with_number: bool=False):
         if document and document.metadata and document.page_content:
             if document.metadata['type'] and document.metadata['type'].startswith("H"):
                 return "\n" \
@@ -244,15 +249,9 @@ class IntelliDocuments():
             if doc.metadata and doc.metadata['id']:
                 # 如果 ID 以 '.0' 结尾，就去掉 '.0'
                 id = doc.metadata['id'][:-2] if doc.metadata['id'].endswith('.0') else doc.metadata['id']
+                if to_id.startswith(id):
+                    md += self.get_node_text(doc, True)
 
-                # 检查是否是兄弟节点或祖先节点
-                id_parts = id.split('.')
-                ancestor_id = ".".join(ancestor_ids)
-                parent_id = ".".join(id_parts[:-1])
-                to_compare = parent_id if len(id_parts)==len(ancestor_ids) + 1 else id
-                if ancestor_id.startswith(to_compare):
-                    md += self._get_text(doc, True)
-        
         return md
 
     def get_prev_markdown(self, to_id="9999999", k=1500, with_number: bool=False):
@@ -265,7 +264,7 @@ class IntelliDocuments():
         # 从后往前遍历文档列表
         for doc in reversed(self.documents):
             if is_prev_id(doc.metadata['id'], to_id):
-                text = self._get_text(doc, with_number)
+                text = self.get_node_text(doc, with_number)
                 length += len(text)
                 md = text + md
 
