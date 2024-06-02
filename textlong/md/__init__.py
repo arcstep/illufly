112f74e16f2a12c8876e0ecd03609d437397bba3
arcstep <43801@qq.com>
2024-06-02 17:59:53 +0800

    add Summarise to writing module

--- textlong/md/__init__.py
+++ textlong/md/__init__.py
@@ -1,3 +1,3 @@
 from .agent import create_agent
 from .documents import IntelliDocuments
-from .writing import BaseWriting, Outline, Detail, Fetch, Translate
+from .writing import BaseWriting, Outline, Detail, Fetch, Translate, Summarise
