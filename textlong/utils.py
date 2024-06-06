0d8fe1e28e208f182eb26e5dafe5bcbaa3af2a53
arcstep <43801@qq.com>
2024-06-06 14:55:43 +0800

    refactor: optimize markdown document generation logic

--- textlong/utils.py
+++ textlong/utils.py
@@ -1,5 +1,10 @@
+from typing import List, Union
+from langchain_core.documents import Document
+
 def raise_not_install(packages):
     print(f"please install package: '{packages}' with pip or poetry")
     # auto install package
     # subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
 
+def markdown(docs: List[Document], sep: str=""):
+    return sep.join([d.page_content for d in docs])
