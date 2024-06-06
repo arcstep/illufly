afc5656a6a0996f4c3e7bc912318c099cbab3c8d
arcstep <43801@qq.com>
2024-06-06 19:48:16 +0800

    add some TODOs for Writing class

--- textlong/md/writing.py
+++ textlong/md/writing.py
@@ -1,6 +1,5 @@
 import copy
 from typing import Union, List
-from abc import ABC, abstractclassmethod
 from langchain_core.documents import Document
 from langchain.globals import set_verbose, get_verbose
 from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
@@ -31,7 +30,7 @@ def call_markdown_chain(chain, input):
     print(f"\n\n实际字数: {len(text)}")
     return MarkdownOutputParser().invoke(text)[0]
 
-class Writing(ABC):
+class Writing():
     """
     基本写作。
     """
@@ -76,9 +75,39 @@ class Writing(ABC):
         """
         self.ref_docs = copy.deepcopy(self.todo_docs)
         return self.ref_docs.documents
+    
+    def save(self):
+        """
+        保存
+        TODO: 支持按用户
+        TODO: 序列化到文件
+        TODO: 序列化到数据库
+        """
+        pass
+
+    def load(self):
+        """
+        加载
+        TODO: 支持按用户
+        TODO: 从文件反序列化
+        TODO: 从数据库反序列化
+        """
+        pass
+
+    def load_prompts(self):
+        """
+        加载提示语模板
+        TODO: 支持按用户加载默认提示语模板
+        TODO: 初始化时将默认提示语模板路径修改为用户指定的模板
+        """
+        pass
 
     def idea(self, task: str, template_id: str=None):
-        """创意"""
+        """
+        创意
+        TODO: 当存在ref_docs时，参考已有文档修改
+        TODO: 根据任务要求推理，选择不同模板
+        """
         prompt = load_prompt(template_id or "创意")
         chain = create_chain(self.llm, prompt)
         resp_md = call_markdown_chain(chain, {"task": task})
@@ -87,11 +116,20 @@ class Writing(ABC):
         return self.todo_docs.documents
     
     def outline(self, task: str, template_id: str=None):
-        """提纲"""
+        """
+        提纲
+        TODO: 当存在ref_docs时，针对扩写要求放大提纲
+        TODO: 当指定局部修改时
+        TODO: 根据任务要求推理，选择不同模板
+        """
         return self.idea(task, template_id or "提纲")
 
     def detail(self, task: str=None, template_id: str=None):
-        """扩写"""
+        """
+        扩写
+        TODO: 当指定局部修改时
+        TODO: 根据任务要求推理，选择不同模板
+        """
         if not self.ref_docs.documents:
             raise ValueError("必须提供《参考提纲》作为扩写依据")
 
@@ -118,8 +156,53 @@ class Writing(ABC):
 
 
     def fetch(self, task: str, template_id: str=None):
+        """
+        提取
+        TODO: 将提取出的结果放入 knowledge 字典，供模板变量引用
+        TODO: 按提取意图和滚动上下文窗口提取长文档
+        TODO: 当指定局部修改时
+        TODO: 默认提取摘要，通过任务意图指定其他提取目标（知识三元组、人物、工作流程等）
+        TODO: 根据任务要求推理，选择不同模板
+        """
         return self.idea(task, template_id or "摘要")
         
     def translate(self):
+        """
+        翻译
+        TODO: 按翻译意图和滚动上下文窗口翻译长文档
+        TODO: 当指定局部修改时
+        """
+        pass
+
+    def refine(self):
+        """
+        修改
+        TODO: 按修改意图和滚动上下文窗口修改长文档，例如替换文中的产品名称
+        TODO: 当指定局部修改时
+        """
+        pass
+
+    def embedding(self):
+        """
+        向量编码
+        TODO: 对todo_docs做向量编码，并保存到向量数据库
+        TODO: 按 Document 缓存编码结果
+        """
+        pass
+
+    def search(self):
+        """
+        相似性查询
+        TODO: 对文档列表做向量相似性查询
+        TODO: 将本地文档文件资料作为向量相似性查询依据
+        TODO: 将查询结果作为创作依据(knowledge变量)
+        """
+        pass
+
+    def chat(self):
+        """
+        对话
+        TODO: RAG对话
+        """
         pass
 
