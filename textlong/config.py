9ee896b4c19f756d79b90c9da32be5c7723dd728
arcstep <43801@qq.com>
2024-06-07 02:10:01 +0800

    refactor: add default values for session, user, and public in config.py

--- textlong/config.py
+++ textlong/config.py
@@ -9,9 +9,22 @@ _QA_FOLDER_NAME = "__QA__"
 _DOCS_FOLDER_NAME = "__DOCS__"
 _TEMP_FOLDER_NAME = "__TEMP__"
 
+def get_default_session():
+    """默认的用户名"""
+    return os.getenv("TEXTLONG_DEFAULT_SESSION") or "default"
+
+def get_default_user():
+    """默认的用户名"""
+    return os.getenv("TEXTLONG_DEFAULT_USER") or "default_user"
+
+def get_default_public():
+    """默认的公共资料存储目录"""
+    return os.getenv("TEXTLONG_PUBLIC") or "public"
+
 def get_textlong_folder():
     """从环境变量中获得项目的存储目录"""
     return os.getenv("TEXTLONG_FOLDER") or "textlong_data"
 
 def get_textlong_doc(filename: str, folder_name: str = ""):
     return os.path.join(get_textlong_folder(), folder_name, filename)
+
