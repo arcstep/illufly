fcacb48c2c447fc4f19c7c9b6eaf2a6947d1717d
arcstep <43801@qq.com>
2024-06-07 22:15:30 +0800

    refactor: add methods instead of Writing class

--- textlong/md/__init__.py
+++ textlong/md/__init__.py
@@ -1,3 +1,3 @@
 from .agent import create_agent
 from .documents import IntelliDocuments
-from .writing import Writing
+from .writing import idea, rewrite, fetch, translate, outline, outline_self, outline_detail
--- textlong/md/writing.py
+++ textlong/md/writing.py
@@ -1,5 +1,6 @@
 import copy
 from typing import Union, List
+from langchain_core.runnables import Runnable
 from langchain_core.documents import Document
 from langchain.globals import set_verbose, get_verbose
 from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
@@ -27,251 +28,115 @@ def call_markdown_chain(chain, input):
         text += chunk.content
         print(chunk.content, end="")
 
-    print("\033[32m" + f"\n\n实际字数: {len(text)}" + "\033[0m")  # 绿色
+    print("\033[32m" + f"\n\n生成{len(text)}字。" + "\033[0m")  # 绿色
     return MarkdownOutputParser().invoke(text)[0]
 
-class Writing():
+def idea(task: str, llm: Runnable, template_id: str=None, ref_doc: str=None):
     """
-    写作。
+    创意
+    """
+    prompt = load_prompt(template_id or "IDEA")
+    doc = f'你已经完成的创作如下：\n{ref_doc}' if ref_doc != None else ''
+    chain = create_chain(llm, prompt, todo_doc=doc)
+    resp_md = call_markdown_chain(chain, {"task": task})
+
+    return resp_md
+
+def outline(task: str, llm: Runnable, template_id: str=None, ref_doc: str=None):
+    """
+    提纲
+    """
+    _template_id = template_id or "OUTLINE"
+    return idea(task=task, llm=llm, template_id=_template_id, ref_doc=ref_doc)
+
+def outline_detail(ref_doc: str, llm: Runnable, template_id: str=None, task: str=None):
+    """
+    扩写
+    """
+    todo_docs = IntelliDocuments(ref_doc)
+    prompt = load_prompt(template_id or "OUTLINE_DETAIL")
+
+    for doc in todo_docs.get_outline_task():
+        prev_doc = markdown(todo_docs.get_prev_documents(doc))
+        next_doc = markdown(todo_docs.get_next_documents(doc))
+        chain = create_chain(
+            llm,
+            prompt,
+            prev_doc=prev_doc,
+            next_doc=next_doc,
+            todo_doc=f'>->>>\n{doc.page_content}<-<<<\n\n'
+        )
+
+        task_howto = f"请仅针对上述`>->>>`和`<-<<<`包围的部份扩写。{task or ''}"
+        resp_md = call_markdown_chain(chain, {"task": task_howto})
+        reply_docs = parse_markdown(resp_md)
+        todo_docs.replace_documents(index_doc=doc, docs=reply_docs)
+
+    return todo_docs.markdown
+
+def outline_self(ref_doc: str, llm: Runnable, template_id: str=None, task: str=None):
+    """
+    丰富提纲
+    """
+    _template_id = template_id or "OUTLINE_SELF"
+    return outline_detail(ref_doc, llm, _template_id, task)
+
+def fetch(ref_doc: str, llm: Runnable, template_id: str=None, task: str=None, k: int=1000):
+    """
+    提取
+
+    - 按任务意图提取文档
+    - 默认提取`摘要`，可以通过`task`指定知识三元组、人物、工作流程等具体要求
     """
 
-    def __init__(
-        self,
-        todo_docs: Union[str, IntelliDocuments]=None,
-        ref_docs: Union[str, IntelliDocuments]=None,
-        llm=None,
-        **kwargs
-    ):
+    prompt = load_prompt(template_id or "SUMMARISE")
+    chain = create_chain(llm, prompt, todo_doc=ref_doc)
+    resp_md = call_markdown_chain(chain, {"task": task})
+    return resp_md
 
-        self.llm = llm
+def rewrite(ref_doc: str, llm: Runnable, template_id: str=None, task: str=None, k: int=1000):
+    """
+    修改
 
-        self.knowledge = {}
-        self.save_path = ""
+    - 按修改意图和滚动上下文窗口修改长文档，例如替换文中的产品名称
+    """
+    ref_docs = IntelliDocuments(ref_doc)
+    prompt = load_prompt(template_id or "REWRITE")
 
-        if isinstance(ref_docs, str) or ref_docs == None:
-            self.ref_docs = IntelliDocuments(doc_str=ref_docs)
-        elif isinstance(ref_docs, IntelliDocuments):
-            self.ref_docs = docs
-        else:
-            raise ValueError("ref_docs MUST be str or IntelliDocuments")
-
-        if isinstance(todo_docs, str) or todo_docs == None:
-            self.todo_docs = IntelliDocuments(doc_str=todo_docs)
-        elif isinstance(todo_docs, IntelliDocuments):
-            self.todo_docs = todo_docs
-        else:
-            raise ValueError("todo_docs MUST be str or IntelliDocuments")
-
-    @property
-    def documents(self):
-        return self.todo_docs.documents
-
-    @property
-    def markdown(self):
-        return self.todo_docs.markdown
-
-    def clone(self, new_document_id: str=None):
-        """
-        返回一个新的对象，并确保所有对象都已经深度拷贝。
-        
-        如果不指定 new_document_id 就仅克隆到内存。
-        TODO:
-        """
-        pass
-
-    def save_as_ref(self):
-        """
-        将 todo_docs 保存为 ref_docs
-        
-        常用于先创作提纲，再进行扩写的场景。
-        """
-        self.ref_docs = copy.deepcopy(self.todo_docs)
-        return self.ref_docs.documents
-
-    def save(self):
-        """
-        保存
-        TODO: 支持按用户
-        TODO: 序列化到文件
-        TODO: 序列化到数据库
-        """
-        pass
-
-    def load(self, document_id: str=None):
-        """
-        加载
-        TODO: 支持按用户
-        TODO: 从文件反序列化
-        TODO: 从数据库反序列化
-        """
-        pass
-
-    def load_prompts(self):
-        """
-        加载提示语模板
-        TODO: 支持按用户加载默认提示语模板
-        TODO: 初始化时将默认提示语模板路径修改为用户指定的模板
-        """
-        pass
-
-    def idea(self, task: str, template_id: str=None):
-        """
-        创意
-        
-        - 当存在ref_docs时，将其作为创意参考。
-        - TODO: 根据任务要求推理，选择不同模板
-        """
-        prompt = load_prompt(template_id or "IDEA")
-        doc = f'你已经完成的创作如下：\n{self.ref_docs.markdown}' if self.ref_docs != None else ''
-        chain = create_chain(self.llm, prompt, todo_doc=doc)
-        resp_md = call_markdown_chain(chain, {"task": task})
-        self.todo_docs.import_markdown(resp_md)
-
-        return self.todo_docs
+    resp_md = ""
+    task_docs = []
+    md_len = 0
     
-    def outline(self, task: str, template_id: str=None):
-        """
-        提纲
-        """
-        return self.idea(task, template_id or "OUTLINE")
+    def create_md(docs):
+        md = markdown(docs)
+        if len(md):
+            todo_doc = f'>->>>\n{md}\n<-<<<'
+            prev_doc = markdown(ref_docs.get_prev_documents(docs[0]))
+            chain = create_chain(llm, prompt, prev_doc=prev_doc, todo_doc=todo_doc)
+            task_howto = f"请针对`>->>>`和`<-<<<`包围的部份重写。{task or ''}"
+            return call_markdown_chain(chain, {"task": task_howto})
+        else:
+            return ""
 
-    def outline_self(self, task: str=None, template_id: str=None):
-        """
-        提纲
-        
-        - 当存在ref_docs时，针对扩写要求放大提纲。
-        - TODO: 当指定局部修改时
-        - TODO: 根据任务要求推理，选择不同模板
-        """
-        if not self.ref_docs.documents:
-            raise ValueError("必须提供`ref_docs`作为扩写依据")
+    for doc in ref_docs.documents:
+        md_len += len(doc.page_content)
+        task_docs.append(doc)
+        if md_len <= k:
+            continue
 
-        return self.detail(task, template_id or "OUTLINE_SELF")
-
-    def detail(self, task: str=None, template_id: str=None):
-        """
-        扩写
-        
-        - 必须提供《参考提纲》作为扩写依据。
-        - TODO: 当指定局部修改时
-        - TODO: 根据任务要求推理，选择不同模板
-        """
-        if not self.ref_docs.documents:
-            raise ValueError("必须提供`ref_docs`作为扩写依据")
-
-        self.todo_docs = copy.deepcopy(self.ref_docs)
-        prompt = load_prompt(template_id or "OUTLINE_DETAIL")
-
-        for doc in self.todo_docs.get_outline_task():
-            prev_doc = markdown(self.todo_docs.get_prev_documents(doc))
-            next_doc = markdown(self.todo_docs.get_next_documents(doc))
-            chain = create_chain(
-                self.llm,
-                prompt,
-                prev_doc=prev_doc,
-                next_doc=next_doc,
-                todo_doc=f'>->>>\n{doc.page_content}\n<-<<<'
-            )
-
-            task_howto = f"{task or ''}\n请仅针对上述`>->>>`和`<-<<<`包围的部份扩写。"
-            resp_md = call_markdown_chain(chain, {"task": task_howto})
-            reply_docs = parse_markdown(resp_md)
-            self.todo_docs.replace_documents(index_doc=doc, docs=reply_docs)
-
-        return self.todo_docs
-
-    def fetch(self, task: str=None, template_id: str=None):
-        """
-        提取
-
-        - 按任务意图提取文档
-        - 默认提取`摘要`，可以通过`task`指定知识三元组、人物、工作流程等具体要求
-        - TODO: 根据任务要求推理，选择不同模板
-        """
-        if not self.ref_docs.documents:
-            raise ValueError("必须提供`ref_docs`作为提取目标")
-
-        prompt = load_prompt(template_id or "SUMMARISE")
-        chain = create_chain(self.llm, prompt, todo_doc=self.ref_docs.markdown)
-        resp_md = call_markdown_chain(chain, {"task": task})
-        self.todo_docs.import_markdown(resp_md)
-
-        return self.todo_docs
-        
-    def translate(self, task: str=None, from_lang: str="中文", to_lang: str="英文", template_id: str=None, k: int=1000):
-        """
-        翻译
-        """
-        refine_task = f'请帮我翻译，从{from_lang}到{to_lang}。{task or ""}'
-        return self.refine(refine_task, template_id or "TRANSLATE", k)
-
-    def refine(self, task: str, template_id: str=None, k: int=1000):
-        """
-        修改
-
-        - 按修改意图和滚动上下文窗口修改长文档，例如替换文中的产品名称
-        - TODO: 当指定局部修改时
-        """
-        if not self.ref_docs.documents:
-            raise ValueError("必须提供`ref_docs`作为修改目标")
-
-        prompt = load_prompt(template_id or "REFINE")
-
-        resp_md = ""
-        task_docs = []
+        resp_md += create_md(task_docs) + "\n\n"
         md_len = 0
-        
-        def create_md(docs):
-            md = markdown(docs)
-            if len(md):
-                todo_doc = f'>->>>\n{md}\n<-<<<'
-                prev_doc = markdown(self.ref_docs.get_prev_documents(docs[0]))
-                chain = create_chain(self.llm, prompt, prev_doc=prev_doc, todo_doc=todo_doc)
-                task_howto = f"{task or ''}\n请针对`>->>>`和`<-<<<`包围的部份重写。"
-                return call_markdown_chain(chain, {"task": task_howto})
-            else:
-                return ""
+        task_docs = []
 
-        for doc in self.ref_docs.documents:
-            md_len += len(doc.page_content)
-            task_docs.append(doc)
-            if md_len <= k:
-                continue
+    if task_docs:
+        resp_md += create_md(task_docs)
 
-            resp_md += create_md(task_docs) + "\n\n"
-            md_len = 0
-            task_docs = []
+    return resp_md
 
-        if task_docs:
-            resp_md += create_md(task_docs)
-
-        self.todo_docs.import_markdown(resp_md)
-        return self.todo_docs
-
-    def embedding(self):
-        """
-        向量编码
-
-        - TODO: 对todo_docs做向量编码，并保存到向量数据库
-        - TODO: 按 Document 缓存编码结果
-        """
-        pass
-
-    def search(self):
-        """
-        相似性查询
-
-        - TODO: 对文档列表做向量相似性查询
-        - TODO: 将本地文档文件资料作为向量相似性查询依据
-        - TODO: 将查询结果作为创作依据(knowledge变量)
-        """
-        pass
-
-    def chat(self):
-        """
-        对话
-
-        - TODO: RAG对话
-        """
-        pass
-
+def translate(ref_doc: str, llm: Runnable, template_id: str=None, task: str=None, k: int=1000):
+    """
+    翻译
+    """
+    _template_id = template_id or "TRANSLATE"
+    _task = task or "如果原文为英文，就翻译为中文；如果原文为中文，就翻译为英文。"
+    return refine(ref_doc, llm, _template_id, _task, k)
