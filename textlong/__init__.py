dcfce24512948b81f526ea0ca15e45a15dd5f7cb
arcstep <43801@qq.com>
2024-06-07 22:58:05 +0800

    refactor: fix markdown formatting in writing.py
    Fix the markdown formatting in the `writing.py` file to ensure that the `todo_doc` variable is properly enclosed by `>->>>` and `<<<-<`. This will ensure that the content within these markers is correctly identified for further processing.

--- textlong/__init__.py
+++ textlong/__init__.py
@@ -1,3 +1,14 @@
-from .md import Writing
 from .utils import markdown
 from .hub import load_prompt
+
+from .md.writing import idea, rewrite, fetch, translate, outline, outline_self, outline_detail
+
+__all__ = [
+  "idea",
+  "rewrite",
+  "fetch",
+  "translate",
+  "outline",
+  "outline_self",
+  "outline_detail",
+]
