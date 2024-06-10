26f7ebadb34d50e66b62f7c68a6887044c53e115
arcstep <43801@qq.com>
2024-06-10 14:26:05 +0800

    Rename module name: from md to writing

--- textlong/md/__init__.py
+++
@@ -1,3 +0,0 @@
-from .agent import create_agent
-from .documents import IntelliDocuments
-from .writing import idea, rewrite, fetch, translate, outline, outline_self, outline_detail
---
+++ textlong/writing/__init__.py
@@ -0,0 +1,3 @@
+from .agent import create_agent
+from .documents import IntelliDocuments
+from .base import idea, rewrite, fetch, translate, outline, outline_self, outline_detail
