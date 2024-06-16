e1b7e25750cbd35aef2599c7938b25c61354d5a7
arcstep <43801@qq.com>
2024-06-16 21:03:00 +0800

    Bump version to v0.3.3

--- textlong/project/__init__.py
+++ textlong/project/__init__.py
@@ -1,1 +1,2 @@
 from .base import Project
+from .projects import list_projects, create_project, is_project_existing
--- textlong/utils.py
+++ textlong/utils.py
@@ -9,6 +9,3 @@ def raise_not_install(packages):
 def raise_not_supply_all(info: str, *args):
     if all(arg is None for arg in args):
         raise ValueError(info)
-
-def markdown(docs: List[Document], sep: str=""):
-    return sep.join([d.page_content for d in docs])
