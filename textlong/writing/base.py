498487adf7231cc99cc9f330239e241c0a4c2437
arcstep <43801@qq.com>
2024-06-16 22:23:18 +0800

    refactor: add knowledge support for writing

--- textlong/writing/base.py
+++ textlong/writing/base.py
@@ -45,22 +45,39 @@ def _call_markdown_chain(chain, input):
                 yield buffer
                 buffer = ""
 
-def from_idea(llm: Runnable, task: str=None, prompt_id: str=None, input_doc: str=None, **kwargs):
+def gather_knowledge(knowledge: List[str]):
+    kg_doc = ''
+    if knowledge:
+        if isinstance(knowledge, str):
+            knowledge = [knowledge]
+        kg_doc = '你应当知道这些知识：'
+        kg_doc += '\n'.join([d for d in knowledge])
+    return kg_doc
+
+def from_idea(llm: Runnable, task: str=None, prompt_id: str=None, input_doc: str=None, knowledge: List[str]=None, **kwargs):
     """
     创意：从一个创意开始生成长文档。
     """
+    if not task:
+        raise ValueError("'task' MUST NOT BE EMPTY !")
+
     prompt = load_string_prompt("from_idea", prompt_id or "IDEA")
-    knowledge = f'你已经完成的创作如下：\n{input_doc}' if input_doc != None else ''
-    chain = _create_chain(llm, prompt, knowledge=knowledge, **kwargs)
+    todo_doc = f'你已经完成的创作如下：\n{input_doc}' if input_doc != None else ''
+    kg = gather_knowledge(knowledge)
+    chain = _create_chain(llm, prompt, todo_doc=todo_doc, knowledge=kg, **kwargs)
 
     for delta in _call_markdown_chain(chain, {"task": task}):
         yield delta
 
-def from_outline(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: str=None, **kwargs):
+def from_outline(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: str=None, knowledge: List[str]=None, **kwargs):
     """
     扩写：从大纲扩充到细节。
     """
+    if not input_doc:
+        raise ValueError("'input_doc' MUST NOT BE EMPTY !")
+
     todo_docs = MarkdownDocuments(input_doc)
+    kg = gather_knowledge(knowledge)
     prompt = load_string_prompt("from_outline", prompt_id or "OUTLINE_DETAIL")
 
     last_index = None
@@ -75,8 +92,8 @@ def from_outline(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: 
         # 生成匹配的<OUTLINE/>所在的部份
         prev_doc = MarkdownDocuments.to_markdown(todo_docs.get_prev_documents(doc))
         next_doc = MarkdownDocuments.to_markdown(todo_docs.get_next_documents(doc))
-        knowledge = f'{prev_doc}>->>>\n{doc.page_content}<<<-<\n\n{next_doc}'
-        chain = _create_chain(llm, prompt, knowledge=knowledge, **kwargs)
+        todo_doc = f'{prev_doc}>->>>\n{doc.page_content}<<<-<\n\n{next_doc}'
+        chain = _create_chain(llm, prompt, todo_doc=todo_doc, knowledge=kg, **kwargs)
 
         resp_md = ""
         task_howto = f"请仅针对上述`>->>>`和`<<<-<`包围的部份扩写。{task or ''}"
@@ -89,27 +106,34 @@ def from_outline(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: 
     # 生成最后一个<OUTLINE/>之后的部份
     yield MarkdownDocuments.to_markdown(outline_docs[last_index:None])
 
-def extract(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: str=None, k: int=1000, **kwargs):
+def extract(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: str=None, k: int=1000, knowledge: List[str]=None, **kwargs):
     """
     提取：按任务意图从长文档提取内容。
 
     - 默认提取`摘要`，可以通过`task`指定知识三元组、人物、工作流程等具体要求
     """
+    if not input_doc:
+        raise ValueError("'input_doc' MUST NOT BE EMPTY !")
 
     prompt = load_string_prompt("extract", prompt_id or "SUMMARISE")
-    chain = _create_chain(llm, prompt, knowledge=input_doc, **kwargs)
+    kg = gather_knowledge(knowledge)
+    chain = _create_chain(llm, prompt, todo_doc=input_doc, knowledge=kg, **kwargs)
     resp_md = _call_markdown_chain(chain, {"task": task})
     for chunk in resp_md:
         yield chunk
 
-def from_chunk(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: str=None, k: int=1000, **kwargs):
+def from_chunk(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: str=None, k: int=1000, knowledge: List[str]=None, **kwargs):
     """
     修改：从一个长文档的分段逐个进行处理。
 
     - 例如，例如替换文中的产品名称
     """
+    if not input_doc:
+        raise ValueError("'input_doc' MUST NOT BE EMPTY !")
+
     ref_docs = MarkdownDocuments(input_doc)
     prompt = load_string_prompt("from_chunk", prompt_id or "REWRITE")
+    kg = gather_knowledge(knowledge)
 
     resp_md = ""
     task_docs = []
@@ -119,8 +143,8 @@ def from_chunk(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: st
         md = MarkdownDocuments.to_markdown(docs)
         if len(md):
             prev_doc = MarkdownDocuments.to_markdown(ref_docs.get_prev_documents(docs[0]))
-            knowledge = f'{prev_doc}>->>>\n{md}\n<<<-<'
-            chain = _create_chain(llm, prompt, knowledge=knowledge, **kwargs)
+            todo_doc = f'{prev_doc}>->>>\n{md}\n<<<-<'
+            chain = _create_chain(llm, prompt, todo_doc=todo_doc, knowledge=kg, **kwargs)
             for chunk in _call_markdown_chain(chain, {"task": task or ''}):
                 yield chunk
         else:
