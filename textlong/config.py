24f6929dd11eac25bb1c618f4b7680a1ff064264
arcstep <43801@qq.com>
2024-06-18 18:01:34 +0800

    refactor: Update config.py to use environment variables for folder paths

--- textlong/config.py
+++ textlong/config.py
@@ -1,58 +1,83 @@
 import os
 
-def get_default_session():
-    """默认的用户名"""
-    return os.getenv("TEXTLONG_DEFAULT_SESSION") or "default"
-
-def get_default_user():
-    """默认的用户名"""
-    return os.getenv("TEXTLONG_DEFAULT_USER") or "default_user"
-
-def get_folder_public():
-    """默认的公共资料目录"""
-    return os.getenv("TEXTLONG_PUBLIC") or ""
+def get_folder_root():
+    return get_default_env("TEXTLONG_ROOT")
 
 def get_folder_prompts_chat(action: str):
-    """默认的对话提示语模板目录"""
     return f'{get_folder_chat_prompts()}/{action}'
 
 def get_folder_prompts_string(action: str):
-    """默认的字符串提示语模板目录"""
     return f'{get_folder_string_prompts()}/{action}'
 
 def get_folder_chat_prompts():
-    return os.getenv("TEXTLONG_CHAT_PROMPTS") or "__CHAT_PROMPTS__"
+    return get_default_env("TEXTLONG_CHAT_PROMPTS")
 
 def get_folder_string_prompts():
-    """默认的项目提示语目录"""
-    return os.getenv("TEXTLONG_STR_PROMPTS") or "__STR_PROMPTS__"
+    return get_default_env("TEXTLONG_STR_PROMPTS")
 
 def get_folder_logs():
-    """默认的项目日志目录"""
-    return os.getenv("TEXTLONG_LOGS") or "__LOG__"
+    return get_default_env("TEXTLONG_LOGS")
 
 def get_project_config_file():
-    """默认的项目配置文件"""
-    return os.getenv("TEXTLONG_CONFIG_FILE") or "project_config.yml"
+    return get_default_env("TEXTLONG_CONFIG_FILE")
 
 def get_project_script_file():
-    """默认的项目脚本文件"""
-    return os.getenv("TEXTLONG_SCRIPT_FILE") or "project_script.yml"
-
-def get_folder_root():
-    """从环境变量中获得项目的存储目录"""
-    return os.getenv("TEXTLONG_ROOT") or ""
+    return get_default_env("TEXTLONG_SCRIPT_FILE")
 
 def get_folder_share():
-    """默认的分享目录"""
-    return os.getenv("TEXTLONG_SHARE") or "__SHARE__"
+    return get_default_env("TEXTLONG_SHARE")
 
 def get_folder_history():
-    """默认的分享目录"""
-    return os.getenv("TEXTLONG_MEMORY_HISTORY") or "__MEMORY_HISTORY__"
+    return get_default_env("TEXTLONG_MEMORY_HISTORY")
 
 def get_folder_qa():
-    return os.getenv("TEXTLONG_QA") or "__QA__"
+    return get_default_env("TEXTLONG_QA")
 
 def get_folder_docs():
-    return os.getenv("TEXTLONG_DOCS") or "__DOCS__"
+    return get_default_env("TEXTLONG_DOCS")
+
+def get_default_session():
+    return os.getenv("TEXTLONG_DEFAULT_SESSION") or "default"
+
+def get_default_user():
+    return os.getenv("TEXTLONG_DEFAULT_USER") or "default_user"
+
+def get_folder_public():
+    return os.getenv("TEXTLONG_PUBLIC") or ""
+
+def get_default_env(key: str=None):
+    """
+    环境变量的默认值。
+    """
+    default_values = {
+        # 根文件夹配置
+        "TEXTLONG_ROOT": "",
+
+        # 提示语文件夹
+        "TEXTLONG_CHAT_PROMPTS": "__CHAT_PROMPTS__",
+        "TEXTLONG_STR_PROMPTS": "__STR_PROMPTS__",
+
+        # 项目文件夹
+        "TEXTLONG_LOGS": "__LOG__",
+        "TEXTLONG_SHARE": "__SHARE__",
+        "TEXTLONG_DOCS": "__DOCS__",
+        "TEXTLONG_QA": "__QA__",
+
+        # 项目文件
+        "TEXTLONG_CONFIG_FILE": "project_config.yml",
+        "TEXTLONG_SCRIPT_FILE": "project_script.yml",
+
+        # 对话历史
+        "TEXTLONG_MEMORY_HISTORY": "__MEMORY_HISTORY__",
+
+        # 用户个人文件夹
+        "TEXTLONG_DEFAULT_SESSION": "default",
+        "TEXTLONG_DEFAULT_USER": "default_user",
+
+        # 公共用户
+        "TEXTLONG_PUBLIC": "",
+    }
+    if key:
+        return os.getenv(key) or default_values[key]
+    else:
+        return default_values
