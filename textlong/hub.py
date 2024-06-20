f138dc0c9391b169661a5934ae4b6160b41ca8b1
arcstep <43801@qq.com>
2024-06-20 17:36:38 +0800

    refactor: Add load_prompt function to hub.py for loading prompt templates

--- textlong/hub.py
+++ textlong/hub.py
@@ -1,3 +1,4 @@
+from typing import List
 from importlib.resources import read_text, is_resource
 from langchain.prompts import (
     PromptTemplate,
@@ -15,6 +16,7 @@ from .config import (
     get_folder_prompts_chat,
 )
 import os
+import re
 import json
 
 def find_resource_promopt():
@@ -65,6 +67,69 @@ def load_resource_prompt(action: str, prompt_id: str):
         kwargs[key] = read_text(resource_folder, resource_file) if is_resource(resource_folder, resource_file) else ''
 
     return template.partial(**kwargs)
+
+def _find_prompt_file(prompt_id: str, file_name, template_folder: str=None, sep: str="/", all_path: List[str]=[]):
+    prompt_id = prompt_id.strip(f'{sep}| ')
+    prompt_folder = os.path.join(get_folder_root(), template_folder or "", prompt_id)
+
+    for ext in ['mu', 'mustache', 'txt']:
+        if (file_path := os.path.join(prompt_folder, f'{file_name}.{ext}')) and os.path.exists(file_path):
+            return file_path
+
+    all_path.append(prompt_folder)
+    if not prompt_id:
+        raise ValueError(f"Can't find {file_name}(.mu, .mustache, .txt) in [ {', '.join(all_path)} ]!")
+    return _find_prompt_file(prompt_id.rpartition(sep)[0], file_name, template_folder, sep, all_path)
+
+def load_prompt(prompt_id: str, template_folder: str=None):
+    """
+    从模板文件夹加载提示语模板。
+    
+    提示语模板中的约定：
+    1. 提示语模板支持 f-string(*.txt) 和 mustache(*.mu) 两种语法
+    2. 加载模板时，从模板路径开始寻找，如果找不到会向上查找文件，直至模板根文件夹
+    3. 提示语模板的主文件是 main.txt 或 main.mu，如果都存在就优先用 main.mu
+    4. 预处理 main.mu 中的 {{>include_name}} 语法
+    5. 使用 PromptTemplate 和 core.utils.mustache 处理其他语法
+       这包括 mustache 中的一般变量、partial变量和判断变量是否存在的逻辑等
+    6. 变量命名时的约定
+       - __xxx, 由系统内部的方法填写，请不要试图赋值
+       - _xxx, 可选变量，加载时会被赋默认值""
+       - xxx, 必须填写的变量
+    """
+    prompt_folder = os.path.join(
+        get_folder_root(),
+        template_folder or "",
+        prompt_id
+    )
+    
+    main_prompt = _find_prompt_file(prompt_id, 'main', template_folder)
+    template_format = 'mustache' if main_prompt.endswith('.mu') or  main_prompt.endswith('.mustache') else 'f-string'
+
+    with open(main_prompt, 'r') as f:
+        prompt_str = f.read()
+
+        if template_format == 'mustache':
+            # 替换 {{>include_name}} 变量
+            include_dict = {}
+            matches = re.findall(r'{{>(.*?)}}', prompt_str)
+            for part_name in matches:
+                part_file = _find_prompt_file(prompt_id, part_name, template_folder)
+                with open(part_file, 'r') as f:
+                    include_dict[part_name] = f.read()
+            for part_name, part_str in include_dict.items():
+                prompt_str = prompt_str.replace("{{>" + part_name + "}}", part_str)
+
+        template = PromptTemplate.from_template(prompt_str, template_format=template_format)
+
+        kwargs = {}
+        for key in template.input_variables:
+            if key.startswith('_'):
+                kwargs[key] = ''
+
+        return template.partial(**kwargs)
+
+    raise ValueError(f'无法构建模板：{prompt_id}')
 
 def load_string_prompt(action: str, prompt_id: str, template_folder: str=None):
     """
