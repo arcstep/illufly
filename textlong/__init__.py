b39eb07989aef7ad0bec382bd67bb31b6a37213a
arcstep <43801@qq.com>
2024-06-21 10:54:46 +0800

    refactor: Update config.py to use a unified folder name for prompts

--- textlong/__init__.py
+++ textlong/__init__.py
@@ -1,9 +1,5 @@
-from .hub import load_resource_prompt, load_string_prompt, save_string_prompt
-from .config import get_default_env
 from .writing import from_idea, from_chunk, from_outline, extract
 from .project import Project, list_projects, create_project, is_project_existing
-from .exporter import export_html, export_jupyter
-from .importer import load_markdown
 
 __all__ = [
   "from_idea",
---
+++ textlong/action/__init__.py
@@ -0,0 +1,2 @@
+from .documents import MarkdownDocuments
+from .base import write, replace, split
