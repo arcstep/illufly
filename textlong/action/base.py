0a3ba5143e6a6834418a37ff51eb3573e9d7b458
arcstep <43801@qq.com>
2024-06-22 07:27:17 +0800

    refactor: Update base.py to fix task mode and task todos initialization

--- textlong/action/base.py
+++ textlong/action/base.py
@@ -17,13 +17,13 @@ def _create_chain(llm, prompt_template, **kwargs):
     prompt = prompt_template.partial(**kwargs)
     return prompt | llm
 
-def _call_markdown_chain(chain, input, fake: bool=False, verbose: bool=False):
-    if get_verbose() or fake or verbose:
+def _call_markdown_chain(chain, input, is_fake: bool=False, verbose: bool=False):
+    if get_verbose() or is_fake or verbose:
         # 用蓝色表示提示语模板
         print("\033[34m" + chain.get_prompts()[0].format(**input) + "\033[0m")
 
-    if fake:
-        yield '<<<<< Fake Content >>>>>\n'
+    if is_fake:
+        yield '<<<<< is_fake Content >>>>>\n'
     else:
         for chunk in chain.stream(input):
             yield chunk.content
@@ -63,7 +63,7 @@ def write(
     knowledge: Union[str, List[str]]=None,
     prompt_id: str=None,
     config: Dict[str, Any]=None,
-    fake: bool=False,
+    is_fake: bool=False,
     verbose: bool=False,
     **kwargs
 ):
@@ -81,11 +81,11 @@ def write(
 
     # knowledge
     kg_doc = '\n'.join([gather_docs(knowledge)]) if knowledge else ''
-    
+
     # prompt
     template_folder = config[template_folder] if 'template_folder' in config else None
     prompt = load_prompt(prompt_id or "IDEA", template_folder=template_folder)
-    
+
     if task_mode == 'all':
         _kwargs = {
             "knowledge__": kg_doc,
@@ -93,16 +93,20 @@ def write(
             **kwargs
         }
         chain = _create_chain(llm, prompt, **_kwargs)
-        for delta in _call_markdown_chain(chain, {"task": task}, fake, verbose):
-            yield delta
+        
+        resp_md = ""
+        for delta in _call_markdown_chain(chain, {"task": task}, is_fake, verbose):
+            resp_md += delta
+            yield ('log', delta)
+        yield ('final', resp_md)
 
     elif task_mode == 'document':
         last_index = None
         new_docs = copy.deepcopy(old_docs)
         for doc, index in task_todos:
             if last_index != None:
-                yield "\n"
-            yield MarkdownDocuments.to_markdown(old_docs.documents[last_index:index])
+                yield ('final', "\n")
+            yield ('final', MarkdownDocuments.to_markdown(old_docs.documents[last_index:index]))
             last_index = index + 1
 
             _kwargs = {
@@ -115,13 +119,37 @@ def write(
             chain = _create_chain(llm, prompt, **_kwargs)
 
             resp_md = ""
-            for delta in _call_markdown_chain(chain, {"task": task}, fake, verbose):
-                yield delta
+            for delta in _call_markdown_chain(chain, {"task": task}, is_fake, verbose):
+                yield ('log', delta)
                 resp_md += delta
+            yield ('extract', extract_text(resp_md))
             reply_docs = parse_markdown(extract_text(resp_md))
             new_docs.replace_documents(doc, doc, reply_docs)
 
-        yield MarkdownDocuments.to_markdown(old_docs.documents[last_index:None])
+        yield ('final', MarkdownDocuments.to_markdown(old_docs.documents[last_index:None]))
+
+def stream_log(llm: Runnable, **kwargs):
+    """
+    打印流式日志。
+    
+    接收的流式内容都为形如 (mode, content) 的元组，其中：
+    mode - 值为 final, extract 或 log
+    content - 文本内容
+    
+    log: 流式输出的中间结果，应当与final或extract搭配使用
+    final: 原始的文本结果直接被采纳
+    extract: 原始的文本结果经过脱壳后被采纳，例如在扩写过程中脱去可能存在的 <OUTLINE></OUTLINE>外壳
+    """
+    md = ''
+    for mode, chunk in write(llm, **kwargs):
+        if mode == 'final':
+            md += chunk
+            print(chunk, end="")
+        elif mode == 'extract':
+            md += extract_text(chunk)
+        else:
+            print(chunk, end="")
+    return md
 
 def idea(
     llm: Runnable,
@@ -129,12 +157,9 @@ def idea(
     prompt_id: str=None,
     **kwargs
 ):
+    sep_mode = "all"
     prompt_id = prompt_id or "IDEA"
-    md = ''
-    for chunk in write(llm, task=task, sep_mode="all", prompt_id=prompt_id, **kwargs):
-        md += chunk
-        print(chunk, end="")
-    return extract_text(md)
+    return stream_log(llm, task=task, sep_mode=sep_mode, prompt_id=prompt_id, **kwargs)
 
 def outline(
     llm: Runnable,
@@ -142,12 +167,9 @@ def outline(
     prompt_id: str=None,
     **kwargs
 ):
+    sep_mode = "all"
     prompt_id = prompt_id or "OUTLINE"
-    md = ''
-    for chunk in write(llm, task=task, sep_mode="all", prompt_id=prompt_id, **kwargs):
-        md += chunk
-        print(chunk, end="")
-    return extract_text(md)
+    return stream_log(llm, task=task, sep_mode=sep_mode, prompt_id=prompt_id, **kwargs)
 
 def from_outline(
     llm: Runnable,
@@ -155,9 +177,7 @@ def from_outline(
     prompt_id: str=None,
     **kwargs
 ):
+    sep_mode = "outline"
     prompt_id = prompt_id or "OUTLINE_DETAIL"
     md = ''
-    for chunk in write(llm, sep_mode="outline", input=input, prompt_id=prompt_id, **kwargs):
-        md += chunk
-        print(chunk, end="")
-    return extract_text(md)
+    return stream_log(llm, sep_mode=sep_mode, input=input, prompt_id=prompt_id, **kwargs)
