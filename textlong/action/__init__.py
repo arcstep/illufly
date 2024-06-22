469ab8b807357722aac6db637ba35227fec9cc4f
arcstep <43801@qq.com>
2024-06-22 09:05:18 +0800

    refactor: Remove unused code and files related to writing tools

--- textlong/action/__init__.py
+++
@@ -1,2 +0,0 @@
-from .documents import MarkdownDocuments
-from .base import write, idea, outline, from_outline, outline_from_outline
--- textlong/writing/base.py
+++ textlong/writing/base.py
@@ -1,147 +1,196 @@
+import os
 import copy
-from typing import Union, List
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import Runnable, RunnableGenerator
+from typing import Union, List, Dict, Any
+from langchain_core.runnables import Runnable
 from langchain_core.documents import Document
 from langchain.globals import set_verbose, get_verbose
-from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 
 from .documents import MarkdownDocuments
-from .output_parser import MarkdownOutputParser
 from ..parser import parse_markdown
 from ..hub import load_prompt
+from ..importer import load_markdown
+from ..utils import extract_text
+
 def _create_chain(llm, prompt_template, **kwargs):
     if not llm:
         raise ValueError("LLM can't be None !")
     prompt = prompt_template.partial(**kwargs)
     return prompt | llm
 
-def _call_markdown_chain(chain, input):
-    if get_verbose():
-        print("\033[34m" + "#"*20, "PROMPT BEGIN", "#"*20)  # 蓝色
-        print(chain.get_prompts()[0].format(**input))
-        print("#"*20, "PROMPT END ", "#"*20 + "\033[0m")  # 重置颜色
+def _call_markdown_chain(chain, input, is_fake: bool=False, verbose: bool=False):
+    if get_verbose() or is_fake or verbose:
+        # 用蓝色表示提示语模板
+        print("\033[34m" + chain.get_prompts()[0].format(**input) + "\033[0m")
 
-    for chunk in chain.stream(input):
-        yield chunk.content
+    if is_fake:
+        yield '<<<<< is_fake Content >>>>>\n'
+    else:
+        for chunk in chain.stream(input):
+            yield chunk.content
 
-def gather_knowledge(knowledge: List[str]):
-    kg_doc = ''
-    if knowledge:
-        if isinstance(knowledge, str):
-            knowledge = [knowledge]
-        kg_doc = '\n>>>>>>>>> 你必须知晓：\n'
-        kg_doc += '\n'.join([d for d in knowledge])
-    return kg_doc + "<<<<<<<<<"
-
-def from_idea(llm: Runnable, task: str=None, prompt_id: str=None, input_doc: str=None, knowledge: List[str]=None, template_folder: str=None, **kwargs):
+def gather_docs(input: Union[str, List[str]]):
     """
-    创意：从一个创意开始生成长文档。
-    """
-    if not task:
-        raise ValueError("'task' MUST NOT BE EMPTY !")
-
-    prompt = load_prompt("from_idea", prompt_id or "IDEA", template_folder=template_folder)
-    todo_doc = f'你已经完成如下创作：\n{input_doc}' if input_doc != None else ''
-    kg = gather_knowledge(knowledge)
-    chain = _create_chain(llm, prompt, __todo_doc__=todo_doc, __knowledge__=kg, **kwargs)
-
-    for delta in _call_markdown_chain(chain, {"__task__": task}):
-        yield delta
-
-def from_outline(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: str=None, knowledge: List[str]=None, template_folder: str=None, **kwargs):
-    """
-    扩写：从大纲扩充到细节。
-    """
-    if not input_doc:
-        raise ValueError("'input_doc' MUST NOT BE EMPTY !")
-
-    todo_docs = MarkdownDocuments(input_doc)
-    kg = gather_knowledge(knowledge)
-    prompt = load_prompt("from_outline", prompt_id or "OUTLINE_DETAIL", template_folder=template_folder)
-
-    last_index = None
-    outline_docs = copy.deepcopy(todo_docs.documents)
-    for doc, index in todo_docs.get_outline_task():
-        # 生成<OUTLINE/>之前的部份
-        if last_index != None:
-            yield "\n"
-        yield MarkdownDocuments.to_markdown(outline_docs[last_index:index])
-        last_index = index + 1
-
-        # 生成匹配的<OUTLINE/>所在的部份
-        prev_doc = MarkdownDocuments.to_markdown(todo_docs.get_prev_documents(doc))
-        next_doc = MarkdownDocuments.to_markdown(todo_docs.get_next_documents(doc))
-        todo_doc = f'{prev_doc}>->>>\n{doc.page_content}<<<-<\n\n{next_doc}'
-        chain = _create_chain(llm, prompt, __todo_doc__=todo_doc, __knowledge__=kg, **kwargs)
-
-        resp_md = ""
-        task_howto = f"请仅针对上述`>->>>`和`<<<-<`包围的部份扩写。{task or ''}"
-        for delta in _call_markdown_chain(chain, {"__task__": task_howto}):
-            yield delta
-            resp_md += delta
-        reply_docs = parse_markdown(resp_md)
-        todo_docs.replace_documents(index_doc=doc, docs=reply_docs)
-
-    # 生成最后一个<OUTLINE/>之后的部份
-    yield MarkdownDocuments.to_markdown(outline_docs[last_index:None])
-
-def extract(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: str=None, k: int=1000, knowledge: List[str]=None, template_folder: str=None, **kwargs):
-    """
-    提取：按任务意图从长文档提取内容。
-
-    - 默认提取`摘要`，可以通过`task`指定知识三元组、人物、工作流程等具体要求
-    """
-    if not input_doc:
-        raise ValueError("'input_doc' MUST NOT BE EMPTY !")
-
-    prompt = load_prompt("extract", prompt_id or "SUMMARISE", template_folder=template_folder)
-    kg = gather_knowledge(knowledge)
-    chain = _create_chain(llm, prompt, __todo_doc__=input_doc, __knowledge__=kg, **kwargs)
-    resp_md = _call_markdown_chain(chain, {"__task__": task})
-    for chunk in resp_md:
-        yield chunk
-
-def from_chunk(llm: Runnable, input_doc: str=None, prompt_id: str=None, task: str=None, k: int=1000, knowledge: List[str]=None, template_folder: str=None, **kwargs):
-    """
-    修改：从一个长文档的分段逐个进行处理。
-
-    - 例如，例如替换文中的产品名称
-    """
-    if not input_doc:
-        raise ValueError("'input_doc' MUST NOT BE EMPTY !")
-
-    ref_docs = MarkdownDocuments(input_doc)
-    prompt = load_prompt("from_chunk", prompt_id or "REWRITE", template_folder=template_folder)
-    kg = gather_knowledge(knowledge)
-
-    resp_md = ""
-    task_docs = []
-    md_len = 0
+    从input收集文本，有三种情况：
+    - 文本字符串
+    - 一个包含文本的文件，一般为md格式
+    - 一组包含文本的文件，一般为md格式
     
-    def create_md(docs):
-        md = MarkdownDocuments.to_markdown(docs)
-        if len(md):
-            prev_doc = MarkdownDocuments.to_markdown(ref_docs.get_prev_documents(docs[0]))
-            todo_doc = f'{prev_doc}>->>>\n{md}\n<<<-<'
-            chain = _create_chain(llm, prompt, __todo_doc__=todo_doc, __knowledge__=kg, **kwargs)
-            for chunk in _call_markdown_chain(chain, {"__task__": task or ''}):
-                yield chunk
+    TODO: 支持 word、html 等其他格式
+    """
+
+    md = ''
+
+    if isinstance(input, str):
+        if input.endswith(".md") and os.path.exists(input):
+            input = [input]
         else:
-            yield ""
+            md = input
 
-    for doc in ref_docs.documents:
-        md_len += len(doc.page_content)
-        task_docs.append(doc)
-        if md_len <= k:
-            continue
+    if isinstance(input, list):
+        for path in input:
+            if isinstance(path, str) and path.endswith(".md") and os.path.exists(path):
+                d = load_markdown(path)
+                md += "\n\n" + d.markdown
+            else:
+                raise ValueError(f"Input File Not Exist: {path}")
 
-        for delta in create_md(task_docs):
-            yield delta
-        md_len = 0
-        task_docs = []
+    return md
 
-    if task_docs:
-        for delta in create_md(task_docs):
-            yield delta
+def write(
+    llm: Runnable,
+    task: str=None,
+    input: Union[str, List[str]]=None,
+    sep_mode: str='all',
+    replace: bool=True,
+    knowledge: Union[str, List[str]]=None,
+    prompt_id: str=None,
+    config: Dict[str, Any]=None,
+    is_fake: bool=False,
+    verbose: bool=False,
+    **kwargs
+):
+    """
+    创作长文。
+    
+    TODO: 支持更多任务拆分模式
+    """
+    config = config or {}
 
+    # input
+    input_doc = gather_docs(input)
+    task_mode, task_todos, old_docs = 'all', [], []
+    if input_doc:
+        old_docs = MarkdownDocuments(input_doc)
+        task_mode, task_todos = old_docs.get_todo_documents(sep_mode)
+
+    # knowledge
+    kg_doc = '\n'.join([gather_docs(knowledge)]) if knowledge else ''
+
+    # prompt
+    template_folder = config[template_folder] if 'template_folder' in config else None
+    prompt = load_prompt(prompt_id or "IDEA", template_folder=template_folder)
+
+    if task_mode == 'all':
+        _kwargs = {
+            "knowledge__": kg_doc,
+            "todo_doc__": '\n'.join([d.page_content for d in task_todos]),
+            **kwargs
+        }
+        chain = _create_chain(llm, prompt, **_kwargs)
+        
+        resp_md = ""
+        for delta in _call_markdown_chain(chain, {"task": task}, is_fake, verbose):
+            resp_md += delta
+            yield ('log', delta)
+        yield ('final', resp_md)
+
+    elif task_mode == 'document':
+        last_index = None
+        new_docs = copy.deepcopy(old_docs)
+        for doc, index in task_todos:
+            if last_index != None:
+                yield ('final', "\n")
+            yield ('final', MarkdownDocuments.to_markdown(old_docs.documents[last_index:index]))
+            last_index = index + 1
+
+            _kwargs = {
+                "knowledge__": kg_doc,
+                "todo_doc__": doc.page_content,
+                "prev_doc__": MarkdownDocuments.to_markdown(new_docs.get_prev_documents(doc)),
+                "next_doc__": MarkdownDocuments.to_markdown(new_docs.get_next_documents(doc)),
+                **kwargs
+            }
+            chain = _create_chain(llm, prompt, **_kwargs)
+
+            resp_md = ""
+            for delta in _call_markdown_chain(chain, {"task": task}, is_fake, verbose):
+                yield ('log', delta)
+                resp_md += delta
+            yield ('extract', extract_text(resp_md))
+            reply_docs = parse_markdown(extract_text(resp_md))
+            new_docs.replace_documents(doc, doc, reply_docs)
+
+        yield ('final', MarkdownDocuments.to_markdown(old_docs.documents[last_index:None]))
+
+def stream_log(llm: Runnable, start_marker: str=None, end_marker: str=None, **kwargs):
+    """
+    打印流式日志。
+    
+    接收的流式内容都为形如 (mode, content) 的元组，其中：
+    mode - 值为 final, extract 或 log
+    content - 文本内容
+    
+    log: 流式输出的中间结果，应当与final或extract搭配使用
+    final: 原始的文本结果直接被采纳
+    extract: 原始的文本结果经过脱壳后被采纳，例如在扩写过程中脱去可能存在的 <OUTLINE></OUTLINE>外壳
+    """
+    md = ''
+    for mode, chunk in write(llm, **kwargs):
+        if mode == 'final':
+            md += chunk
+            print(chunk, end="")
+        elif mode == 'extract':
+            md += extract_text(chunk, start_marker, end_marker)
+        else:
+            print(chunk, end="")
+    return md
+
+def idea(
+    llm: Runnable,
+    task: str,
+    prompt_id: str=None,
+    **kwargs
+):
+    sep_mode = "all"
+    prompt_id = prompt_id or "IDEA"
+    return stream_log(llm, task=task, sep_mode=sep_mode, prompt_id=prompt_id, **kwargs)
+
+def outline(
+    llm: Runnable,
+    task: str,
+    prompt_id: str=None,
+    **kwargs
+):
+    sep_mode = "all"
+    prompt_id = prompt_id or "OUTLINE"
+    return stream_log(llm, task=task, sep_mode=sep_mode, prompt_id=prompt_id, **kwargs)
+
+def from_outline(
+    llm: Runnable,
+    input: Union[str, List[str]],
+    prompt_id: str=None,
+    **kwargs
+):
+    sep_mode = "outline"
+    prompt_id = prompt_id or "OUTLINE_DETAIL"
+    return stream_log(llm, sep_mode=sep_mode, input=input, prompt_id=prompt_id, **kwargs)
+
+def outline_from_outline(
+    llm: Runnable,
+    input: Union[str, List[str]],
+    prompt_id: str=None,
+    **kwargs
+):
+    sep_mode = "outline"
+    prompt_id = prompt_id or "OUTLINE_SELF"
+    return stream_log(llm, "<OUTLINE-MORE>", "</OUTLINE-MORE>", sep_mode=sep_mode, input=input, prompt_id=prompt_id, **kwargs)
