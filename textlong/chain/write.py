c98803e5c650dc171dc1de4a5c76d6a85a2fba91
arcstep <43801@qq.com>
2024-06-25 17:18:00 +0800

    refactor: Update create_chain method to support additional arguments

--- textlong/chain/write.py
+++ textlong/chain/write.py
@@ -1,7 +1,15 @@
 from typing import Any, AsyncIterator, Iterator, AsyncIterator, List, Union
 from langchain_core.runnables import Runnable, RunnableGenerator
 from langchain_core.pydantic_v1 import BaseModel, Field
-from ..writing import write, idea, outline, from_outline, more_outline
+from langchain_core.tracers.schemas import Run
+
+from ..writing.base import (
+    stream,
+    get_idea_args,
+    get_outline_args,
+    get_from_outline_args,
+    get_more_outline_args,
+)
 
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
@@ -16,35 +24,38 @@ class writing_input(BaseModel):
     base_folder: str=None
     prompt_id: str=None
 
-def _create_chain(llm: Runnable, writing_func):
+def create_chain(llm: Runnable, writing_func: Run, **kwargs):
+    """
+    构建执行链。
+    """
     def gen(input: Iterator[Any]) -> Iterator[str]:
-        for kwargs in input:
-            for m, x in writing_func(llm, use_yield=True, **kwargs):
-                if m != 'final':
+        for input_args in input:
+            for m, x in writing_func(llm, use_yield=True, **kwargs, **input_args):
+                if m in ['text', 'chunk', 'front_matter']:
                     yield(x)
 
     async def agen(input: AsyncIterator[Any]) -> AsyncIterator[str]:
         loop = asyncio.get_running_loop()
-        async for kwargs in input:
+        async for input_args in input:
             func_result = await loop.run_in_executor(
                 executor,
-                lambda: list(writing_func(llm, use_yield=True, **kwargs))
+                lambda: list(writing_func(llm, use_yield=True, **kwargs, **input_args))
             )
             for m, x in func_result:
-                if m != 'final':
+                if m in ['text', 'chunk', 'front_matter']:
                     yield x
 
     return RunnableGenerator(gen, agen).with_types(input_type=writing_input, output_type=Iterator[str])
 
-def create_idea_chain(llm: Runnable):
-    return _create_chain(llm, idea)
+def create_idea_chain(llm: Runnable, prompt_id: str=None, **kwargs):
+    return create_chain(llm, stream, **get_idea_args(prompt_id, **kwargs))
 
-def create_outline_chain(llm: Runnable):
-    return _create_chain(llm, outline)
+def create_outline_chain(llm: Runnable, prompt_id: str=None, **kwargs):
+    return create_chain(llm, stream, **get_outline_args(prompt_id, **kwargs))
 
-def create_from_outline_chain(llm: Runnable):
-    return _create_chain(llm, from_outline)
+def create_from_outline_chain(llm: Runnable, prompt_id: str=None, **kwargs):
+    return create_chain(llm, stream, **get_from_outline_args(prompt_id, **kwargs))
 
-def create_more_outline_chain(llm: Runnable):
-    return _create_chain(llm, more_outline)
+def create_more_outline_chain(llm: Runnable,prompt_id: str=None,  **kwargs):
+    return create_chain(llm, stream, **get_more_outline_args(prompt_id, **kwargs))
 
