dbb586ec257abd3123b5d4c4f3cf985f074d0a61
arcstep <43801@qq.com>
2024-07-03 00:25:38 +0800

    refactor: Update base.py to use "chat" instead of "idea" for writing command

--- textlong/__init__.py
+++ textlong/__init__.py
@@ -1,9 +1,10 @@
-from .writing import chat, outline, more_outline, from_outline
+from .writing import chat, idea, outline, more_outline, from_outline
 from .project import WritingProject, list_projects, init_project, is_project_existing
 from .chain import create_chain
 
 __all__ = [
   "chat",
+  "idea",
   "outline",
   "more_outline",
   "from_outline",
