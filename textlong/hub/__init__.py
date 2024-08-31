307dd1891335a63e08393569924c93b7cb9bbf94
arcstep <43801@qq.com>
2024-08-31 15:56:28 +0800

    feat: 优化代码，将block_type转换为小写字母

--- textlong/hub/__init__.py
+++ textlong/hub/__init__.py
@@ -1,6 +1,11 @@
-from .resource import (
+from .str_template import (
   load_resource_prompt,
   load_prompt,
   save_prompt,
   clone_prompt
+)
+
+from .chat_template import (
+  load_resource_chat_template,
+  load_chat_template
 )
