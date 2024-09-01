1acee8af6c667a80745dc1374f0798d32f553a57
arcstep <43801@qq.com>
2024-09-01 16:38:59 +0800

    feat: 添加chk_tail函数到chk.py文件

--- textlong/writing/base.py
+++ textlong/writing/base.py
@@ -13,13 +13,13 @@ from langchain.memory.chat_memory import BaseChatMemory
 
 from .markdown import MarkdownLoader
 from .command import Command
-from ..message import TextBlock
+from ..io import TextBlock
 from ..parser import parse_markdown, create_front_matter
 from ..hub import load_prompt
 from ..importer import load_markdown
 from ..utils import extract_text, safety_path, compress_text
 from ..config import get_env
-from ..message import stream_log
+from ..io import stream_log
 
 def _create_chain(llm, prompt_template, **kwargs):
     if not llm:
