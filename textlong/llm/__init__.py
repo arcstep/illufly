dfe4e67c555e2638300062abb1d08b61aea774bf
arcstep <43801@qq.com>
2024-09-02 17:08:43 +0800

    feat: 从 chat 和 write 中提炼 _call 公共函数

--- textlong/llm/__init__.py
+++ textlong/llm/__init__.py
@@ -1,2 +1,2 @@
-from .base import chat, write, stream
+from .base import new_chat, chat, write
 from .fake import fake_llm
--- textlong/llm/base.py
+++ textlong/llm/base.py
@@ -17,14 +17,16 @@ from ..config import get_env
 from ..io import stream_log, chk_tail
 from ..hub import create_prompt, load_chat_template
 
-def chat(llm, question:str, messages:List=[], toolkits=None, k=10, is_fake=False, **model_kwargs):
+def chat(llm, question:str, messages:List=[], state:Dict={}, toolkits=None, k=10, is_fake=False, **model_kwargs):
     """
-    基于`memory`中的聊天历史，开始多轮对话。
+    基于`messages`中的聊天历史，开始多轮对话。
+    在`state`中管理需要一直保留的变量、成果等，例如数据、提纲等。
 
     Args:
     - llm: 调用模型的函数
     - question: 用户追问的问题
     - messages: 记忆存储器
+    - state: 工作台的状态变量管理
     - k: 保留的历史消息轮数，每轮为2条消息
     - model_kwargs: 模型调用的其他参数
     """
@@ -42,6 +44,55 @@ def chat(llm, question:str, messages:List=[], toolkits=None, k=10, is_fake=False
         final_k = 2*k-1 if k >= 1 else 1
         new_messages = messages[-final_k:]
 
+    return _call(llm, toolkits, messages, new_messages, **model_kwargs)
+
+def new_chat(messages:List=[]):
+    """
+    清空聊天历史记录。
+    TODO: 后续扩展为支持多版本管理。
+    """
+    messages.clear()
+
+def write(llm, prompt_id: str=None, input:Dict[str, Any]={}, messages:List=None, state:Dict={}, toolkits=None, question:str=None, **model_kwargs):
+    """
+    `messages`中的记录将被清空，但会写入新的生成记录。
+    """
+    if not question:
+        question = get_env("TEXTLONG_USER_MESSAGE_DEFAULT")
+
+    template = load_chat_template(prompt_id)
+    system_prompt = template.format(**input)
+
+    # 重置消息列表
+    new_chat(messages)
+    # 构造 system_prompt
+    messages.extend([
+        {
+            'role': 'system',
+            'content': system_prompt
+        },
+        {
+            'role': 'user',
+            'content': question
+        }
+    ])
+
+    new_messages = messages[None:None]
+
+    return _call(llm, toolkits, messages, new_messages, **model_kwargs)
+
+def _call(llm, toolkits, messages, new_messages, **model_kwargs):
+    """
+    调用大模型。
+
+    Args:
+    - llm: 调用模型的函数
+    - toolkits: 工具包
+    - messages: 完整的消息列表
+    - new_messages: 对话时代入提示语中的消息列表
+    - model_kwargs: 模型调用的其他参数
+
+    """
     # 调用大模型
     while(True):
         to_continue_call_llm = False
@@ -82,129 +133,3 @@ def chat(llm, question:str, messages:List=[], toolkits=None, k=10, is_fake=False
             continue
         else:
             return log['output']
-
-def write(llm, prompt_id: str=None, input:Dict[str, Any]={}, messages:List=None, question:str=None, **model_kwargs):
-    if not question:
-        question = get_env("TEXTLONG_USER_MESSAGE_DEFAULT")
-
-    template = load_chat_template(prompt_id)
-    system_prompt = template.format(**input)
-
-    # 重置消息列表
-    messages.clear()
-    # 构造 system_prompt
-    messages.extend([
-        {
-            'role': 'system',
-            'content': system_prompt
-        },
-        {
-            'role': 'user',
-            'content': question
-        }
-    ])
-
-    # 调用大模型
-    log = stream_log(llm, messages, **model_kwargs)
-    messages.extend([{
-        'role': 'assistant',
-        'content': log['output']
-    }])
-
-    # 补充校验的尾缀
-    stream_log(chk_tail, log['output'])
-    
-    return log['output']
-
-def stream(model_call, prompt_id:str=None, question:str=None, memory:BaseChatMemory=None, input:Dict[str, Any]={}, **model_kwargs):
-    """
-    stream 实现了大模型调用的逻辑封装，包括：提示语模板、模型和基于追问的记忆管理。
-
-    请注意记忆管理逻辑：如果调用不是制定了quesiton参数来追问，则记忆会被清空。
-    这个「奇怪」的设计也可以被当作清空聊天记录的技巧。
-
-    当然，这个设计实际上并不「奇怪」，而是专门为长文档写作的场景考虑而设计。
-    当你在生成长文档时，通常并不需要根据聊天历史记录，而是根据模板中要求填写的参数来生成文档；
-    相反的，如果你需要针对已经生成的长文档成果来讨论，则可能需要一直保持聊天历史记录。
-
-    在实际使用中，question 可以在第一次写作时就同时提出，也可以在后续追问时提出。
-
-    Args:
-    - model_call: 调用的模型函数
-    - prompt_id: 提示语模板ID
-    - input: 提示语模板的输入参数字典
-    - question: 用户追问的问题
-    - memory: 记忆存储器
-    - model_kwargs: 模型调用的其他参数
-    """
-    # 构建提示语
-    history = get_raw_messages(memory)
-    prompt = create_prompt(prompt_id, question, history, **input)
-
-    # 调用大模型
-    output_text = stream_log(model_call, prompt, **model_kwargs)
-
-    # 整理记忆
-    if memory:
-        # 没有指定问题时，则清空记忆
-        if not question:
-            question = get_env("TEXTLONG_USER_MESSAGE_DEFAULT")
-            memory.chat_memory.clear()
-
-        if len(memory.chat_memory.messages) == 0:
-            memory.chat_memory.add_message(SystemMessage(prompt[0]["content"]))
-            memory.chat_memory.add_message(HumanMessage(prompt[1]["content"]))
-        else:
-            memory.chat_memory.add_message(HumanMessage(question))
-
-        memory.chat_memory.add_message(AIMessage(output_text))
-
-    # 生成哈希值
-    # 移除前后空格以确保唯一性
-    trimmed_output_text = output_text.strip()
-    hash_object = hashlib.sha256(trimmed_output_text.encode())
-    hash_hex = hash_object.hexdigest()  # 获取十六进制哈希值
-    # 转换为8位数字哈希值
-    hash_code = int(hash_hex, 16) % (10 ** 8)  # 取模运算得到8位数字
-
-    tail = f'>-[END]>> 【{get_env("TEXTLONG_AIGC_INFO_DECLARE")}，{get_env("TEXTLONG_AIGC_INFO_CHK")} {hash_code}】'
-    print(tail)
-    
-    return output_text
-
-def get_raw_messages(memory):
-    """
-    从记忆对象构造用于大模型调用的消息列表。
-
-    以下是检查合法性的准则：
-    1. role 必须是 ["user", "assistant", "system", "function", "plugin", "tool"] 其中之一
-    2. 最后一条消息的 role 必须是 ["user", "function", "tool"] 其中之一
-    3. 返回时剔除 role 为 system 的消息
-    """
-    if not memory:
-        return []
-
-    # messages = memory.chat_memory.messages
-    messages = memory
-
-    string_messages = []
-    selected_messages = messages[:2] + messages[2:][-20:]
-    for m in selected_messages:
-        if isinstance(m, HumanMessage):
-            role = "user"
-        elif isinstance(m, AIMessage):
-            role = "assistant"
-        elif isinstance(m, ChatMessage):
-            role = m.role
-        elif isinstance(m, SystemMessage):
-            role = "System"
-            continue
-        else:
-            raise ValueError(f"Got unsupported message type: {m}")
-
-        string_messages.append({
-            'role': role,
-            'content': m.content
-        })
-
-    return string_messages
