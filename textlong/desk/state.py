7381896007e9fde861e1f031b7eb0a26023ef122
arcstep <43801@qq.com>
2024-09-05 11:35:30 +0800

    feat: 支持扩写

--- textlong/desk/state.py
+++ textlong/desk/state.py
@@ -5,15 +5,15 @@ from .markdown import Markdown
 class State():
     def __init__(
         self,
-        output: str=None,
+        markdown: str=None,
         outline: Markdown=None,
         from_outline: Dict[str, Document]=None,
         data: Dict[str, Any]=None,
         knowledge: List[str]=None,
         messages: []=None
     ):
-        self.output = output or ''
-        self.outline = outline or Markdown()
+        self.markdown = markdown or Markdown()
+        self.outline = outline or []
         self.from_outline = from_outline or {}
         self.data = data or {}
         self.knowledge = knowledge or []
