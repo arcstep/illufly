3211b6f7b40929ac6117681c43f297e05c385fa6
arcstep <43801@qq.com>
2024-09-11 15:34:55 +0800

    feat: 修改文档，从概念上将工作台Desk 替换为智能体 Agent

--- textlong/__init__.py
+++ textlong/__init__.py
@@ -1,12 +1,3 @@
-from .writing import chat, idea, outline, more_outline, from_outline
-from .project import WritingProject, list_projects, init_project, is_project_existing
-from .chain import create_chain
 
 __all__ = [
-  "chat",
-  "idea",
-  "outline",
-  "more_outline",
-  "from_outline",
-  "WritingProject",
 ]
