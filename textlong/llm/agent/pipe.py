7c5332cd6f70b380e785f30b6f0131ac8011701b
arcstep <43801@qq.com>
2024-09-13 09:12:13 +0800

    feat: 修复 PIpe 对 Runnable 类型的兼容

--- textlong/llm/agent/pipe.py
+++ textlong/llm/agent/pipe.py
@@ -11,50 +11,33 @@ class Pipe(Runnable):
 
     Pipe 是 Runnable 的子类，因此可以作为 Runnable 使用。
     """
-    def __init__(self, runnables: List[Union[Runnable, tuple]]=[]):
+    def __init__(self, *runnables):
         """ 
         快速构造智能体运行管道
         """
         super().__init__("PIPE")
-        self.start_runnable = None
-        self.to_runnables = []
-
-        if len(runnables) > 0:
-            self.start(runnables[0])
-            for run in runnables[1:]:
-                if isinstance(run, tuple):
-                    self.to(run[0], run[1])
-                else:
-                    self.to(run, None)
+        self.runnables = runnables
 
     def __str__(self):
-        runnable_types = [type(run).__name__ for run in [self.start_runnable] + self.to_runnables]
+        runnable_types = [type(run).__name__ for run in self.runnables]
         return f"Pipe({runnable_types})"
     
     def __repr__(self):
-        runnable_types = [type(run).__name__ for run in [self.start_runnable] + self.to_runnables]
+        runnable_types = [type(run).__name__ for run in self.runnables]
         return f"Pipe({runnable_types})"
-
-    def start(self, run: Runnable):
-        self.start_runnable = run
-
-    def to(self, run: Runnable, prompt: str):
-        self.to_runnables.append(run)
 
     def call(self, *args, **kwargs):
         """
         执行智能体管道。
         """
-        all_runnables = [self.start_runnable] + self.to_runnables
         prev_runnable = None
-        for index, run in enumerate(all_runnables):
+        for index, run in enumerate(self.runnables):
+            info = self._get_node_info(index + 1, run)
+            yield TextBlock("info", info)
             if index == 0:
-                yield TextBlock("info", f">>> PIPE <{index}> : START")
                 current_args = args
                 current_kwargs = kwargs
             else:
-                info = f">>> PIPE <{index}> : {compress_text(run.system_prompt, 30, 30, 10)}"
-                yield TextBlock("info", info)
                 if isinstance(prev_runnable, Template):
                     prompt = prev_runnable.memory
                 else:
@@ -62,8 +45,18 @@ class Pipe(Runnable):
                 current_args = [prompt]
                 current_kwargs = {}
 
-            self.create_new_memory(f"请节点 <{index}> 处理任务")
+            self.create_new_memory(f"节点 <{index}> 正在处理任务...")
             for block in run.call(*current_args, **current_kwargs):
                 yield block
             self.remember_response(run.output)
             prev_runnable = run
+
+    def _get_node_info(self, index, run):
+        if isinstance(run, ChatAgent):
+            if run.system_prompt:
+                info = f">>> Node {index}: {compress_text(run.system_prompt, 30, 30, 10)}"
+            else:
+                info = f">>> Node {index}: {run.__class__.__name__}"
+        else:
+            info = f">>> Node {index}: {run.__class__.__name__}"
+        return info
