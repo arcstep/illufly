bcdf38fefc3dc13be937336979df09735a25f2c0
arcstep <43801@qq.com>
2024-09-17 23:48:21 +0800

    add: 增加通义万相的「Cosplay」能力工具

--- illufly/tools/__init__.py
+++ illufly/tools/__init__.py
@@ -1,4 +1,4 @@
 from ..core.agent import ToolAgent
 from .python_code import PythonCodeTool
 
-from ..community.dashscope import Text2ImageWanx
+from ..community.dashscope import Text2ImageWanx, CosplayWanx
