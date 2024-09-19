9f8e432ce78f60c6eb3e3373edf16232aa34fab5
arcstep <43801@qq.com>
2024-09-19 14:24:08 +0800

    feat: 将 Tools 管理的方法从 Runnable 转移到 ChatAgent，即只有 ChatAgent 才能使用工具

--- illufly/core/runnable/agent/chat.py
+++ illufly/core/runnable/agent/chat.py
@@ -48,15 +48,47 @@ class ChatAgent(BaseAgent):
     @property
     def toolkits(self):
         return self.tools + self.prepared_tools
-
-    def get_tools_desc(self, tools: List["Runnable"]=None):
+    
+    def get_tools(self, tools: List["Runnable"]=None):
         if tools and (
             not isinstance(tools, list) or
             not all(isinstance(tool, Runnable) for tool in tools)
         ):
             raise ValueError("tools 必须是 Runnable 列表")
-        _tools = tools or []
-        return [t.tool for t in (self.toolkits + _tools)]
+        else:
+            tools = []
+        return self.toolkits + tools
+
+    def get_tools_name(self, tools: List["Runnable"]=None):
+        return ",".join([a.name for a in self.get_tools(tools)])
+
+    def get_tools_desc(self, tools: List["Runnable"]=None):
+        return [t.tool_desc for t in self.get_tools(tools)]
+
+    def get_tools_instruction(self, tools: List["Runnable"]=None):
+        """
+        描述工具调用的具体情况。
+        """
+        action_output = {
+            "index": "integer: index of selected function",
+            "function": {
+                "name": "(string): 填写选中工具的参数名称",
+                "parameters": "(json): 填写具体参数值"
+            }
+        }
+        name_list = ",".join([a.name for a in self.get_tools(tools)])
+        example = '\n'.join([
+            '**工具函数输出示例：**',
+            '```json',
+            '[{"index": 0, "function": {"name": "get_current_weather", "parameters": "{\"location\": \"广州\"}"}},',
+            '{"index": 1, "function": {"name": "get_current_weather", "parameters": "{\"location\": \"上海\"}"}}]',
+            '```'
+        ])
+
+        output = f'```json <tools-calling>\n[{json.dumps(action_output, ensure_ascii=False)}]\n```'
+
+        return f'从列表 [{name_list}] 中选择一个或多个funciton，并按照下面的格式输出函数描述列表，描述每个函数的名称和参数：\n{output}\n{example}'
+
 
     def update_python_code_tool(self):
         from ....tools import PythonCodeTool
