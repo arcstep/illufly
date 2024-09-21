7c6d1e5fe82432b31437fc42d25482d9a091c71a
arcstep <43801@qq.com>
2024-09-21 12:04:56 +0800

    add Importer

--- illufly/importer/md.py
+++ illufly/importer/md.py
@@ -1,14 +1,22 @@
 import os
-from ..types import Markdown
+from ..types import Markdown, Document, TextBlock
+from ..core.runnable import Importer
 
-def load_markdown(file_path: str=None) -> Markdown:
-    """
-    从文件加载文本。
-    """
+class MarkdownFileImporter(Importer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    txt = None
-    if file_path:
-        if os.path.exists(file_path):
-            with open(file_path, 'r', encoding='utf-8') as f:
-                txt = f.read()
-    return Markdown(txt)
+    def call(self, file_path: str=None, **kwargs) -> Markdown:
+        """
+        将文件作为 markdown 文本导入。
+        """
+
+        txt = None
+        if file_path:
+            if os.path.exists(file_path):
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    txt = f.read()
+                    self._output = txt
+                    yield TextBlock("chunk", compress_text(txt))
+        
+        self._output = Markdown(txt).split()
