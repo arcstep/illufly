86763818f6cfd87751c148cafbedf9a76a8861e3
arcstep <43801@qq.com>
2024-09-21 12:04:56 +0800

    add Importer

---
+++ illufly/core/runnable/importer.py
@@ -0,0 +1,14 @@
+from .base import Runnable
+from ...io import TextBlock
+from ...utils import compress_text
+
+class Importer(Runnable):
+    """
+    导入器。
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def call(self, text: str, **kwargs):
+        self._output = text
+        yield TextBlock("chunk", compress_text(text))
