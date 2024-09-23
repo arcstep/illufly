ca9bc5e274e1159d5a28a2c3c4b94d562677753e
arcstep <43801@qq.com>
2024-09-23 15:25:42 +0800

    0.2.1

--- illufly/knowledge/base.py
+++
@@ -1,9 +0,0 @@
-def load_docs(filename: str) -> List[Document]:
-    """
-    按照文档类型加载文档，并直输出循环拆分后的文档块。
-    """
-    file_loader = FileLoadFactory.get_loader(filename)
-    if file_loader:
-        return file_loader.load_and_split(self.text_spliter)
-    else:
-        return []
