108e3826753a0d5f9a959d73de1c1eb1bbd159fd
arcstep <43801@qq.com>
2024-09-25 00:30:27 +0800

    feat: 按照最大行数限定 Embeddings 批次

--- illufly/core/runnable/embeddings/base.py
+++ illufly/core/runnable/embeddings/base.py
@@ -27,11 +27,12 @@ class BaseEmbeddings(Runnable):
     ```
     """
 
-    def __init__(self, model: str=None, api_key: str=None, dim: int=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.dim = dim if dim else 1024
+    def __init__(self, model: str=None, api_key: str=None, dim: int=None, max_lines: int=None, **kwargs):
+        super().__init__(**kwargs)
+        self.dim = dim or 1024
         self.model = model
         self.api_key = api_key
+        self.max_lines = max_lines or 5
         self.clear_output()
 
     def clear_output(self):
@@ -49,7 +50,6 @@ class BaseEmbeddings(Runnable):
     def call(
         self,
         docs: Union[str, List[str], List[Document]],
-        batch_size: int=None,
         **kwargs
     ) -> List[Document]:
         """
@@ -74,46 +74,43 @@ class BaseEmbeddings(Runnable):
             elif not isinstance(d, Document):
                 raise ValueError(f"文档类型错误: {type(d)}")
 
-        yield from self._process_batch(docs, batch_size, vector_folder)
+        yield from self._process_batch(docs, vector_folder)
 
         self._last_output = docs
         return docs
 
-    def _process_batch(self, docs, batch_size, vector_folder):
-        max_batch_size = batch_size or get_env("ILLUFLY_EMBEDDINGS_BATCH_SIZE")
+    def _process_batch(self, docs, vector_folder):
         batch_texts = []
         batch_docs = []
-        texts_size = 0
 
-        for d in docs:
-            vector_path = hash_text(d.text) + ".emb"
-            source = clean_filename(d.metadata['source'])
-            cache_path = os.path.join(vector_folder, source or "no_source", vector_path)
+        for i in range(0, len(docs), self.max_lines):
+            batch = docs[i:i + self.max_lines]
+            batch_texts = [d.text for d in batch]
+            batch_docs = batch
 
-            if os.path.exists(cache_path) and os.path.getsize(cache_path) > 0:
-                with open(cache_path, 'rb') as f:
-                    embeddings = pickle.load(f)
-                    if embeddings:
-                        d.metadata['embeddings'] = embeddings
-                        continue
+            # 检查哪些文件已经存在
+            existing_files = self._check_existing_files(batch_docs, batch_texts, vector_folder)
+            batch_texts = [text for text, exists in zip(batch_texts, existing_files) if not exists]
+            batch_docs = [doc for doc, exists in zip(batch_docs, existing_files) if not exists]
 
-            text_length = len(d.text)
-            if batch_texts and texts_size + text_length > max_batch_size:
-                yield TextBlock("info", f"文本向量转换 {texts_size} 字 / {len(batch_texts)} 个文件")
+            if batch_texts:
+                yield TextBlock("info", f"文本向量转换 {sum(len(d.text) for d in batch_docs)} 字 / {len(batch_docs)} 个文件")
                 vectors = self.embed_documents(batch_texts)
                 yield from self._save_vectors_to_cache(batch_docs, batch_texts, vectors, vector_folder)
-                batch_texts = []
-                batch_docs = []
-                texts_size = 0
 
-            batch_texts.append(d.text)
-            batch_docs.append(d)
-            texts_size += text_length
-
-        if batch_texts:
-            yield TextBlock("info", f"文本向量转换 {texts_size} 字 / {len(batch_texts)} 个文件")
-            vectors = self.embed_documents(batch_texts)
-            yield from self._save_vectors_to_cache(batch_docs, batch_texts, vectors, vector_folder)
+    def _check_existing_files(self, docs, batch_texts, vector_folder):
+        existing_files = []
+        for index, text in enumerate(batch_texts):
+            vector_path = hash_text(text) + ".emb"
+            source = clean_filename(docs[index].metadata['source'])
+            cache_path = os.path.join(vector_folder, source or "no_source", vector_path)
+            if os.path.exists(cache_path):
+                with open(cache_path, 'rb') as f:
+                    docs[index].metadata['embeddings'] = pickle.load(f)
+                existing_files.append(True)
+            else:
+                existing_files.append(False)
+        return existing_files
 
     def _save_vectors_to_cache(self, docs, batch_texts, vectors, vector_folder):
         for index, text in enumerate(batch_texts):
