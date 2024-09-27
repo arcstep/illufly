42581f490a48b4d7864139fd7619a8da88dbed26
arcstep <43801@qq.com>
2024-09-27 12:42:11 +0800

    feat: 为上传到OSS增加缓存功能，每个自然日不重复上传

--- illufly/community/http.py
+++ illufly/community/http.py
@@ -4,6 +4,7 @@ import time
 import requests
 import aiohttp
 import asyncio
+import hashlib
 
 from ..io import TextBlock
 from ..config import get_env
@@ -11,7 +12,38 @@ from ..config import get_env
 CHECK_RESULT_SECONDS = get_env("HTTP_CHECK_RESULT_SECONDS")
 DASHSCOPE_BASE_URL = get_env("DASHSCOPE_BASE_URL")
 
-def confirm_upload_file(model: str=None, image_path: str=None, api_key: str=None):
+# 保留当日上传过的缓存
+# 缓存的命名：以年月作为子目录，将上传内容和日期一起做MD5编码作为文件名称
+
+def get_upload_cache(file_path: str, upload_server: str="aliyun_oss") -> str:
+    UPLOAD_CACHE_DIR = get_env("ILLUFLY_UPLOAD_CACHE_DIR")
+    date_dir = time.strftime("%Y-%m")
+    cache_dir = os.path.join(UPLOAD_CACHE_DIR, date_dir, upload_server)
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            file_content = f.read()
+            content_md5 = hashlib.md5(file_content).hexdigest() + ".json"
+            cache_path = os.path.join(cache_dir, content_md5)
+            if os.path.exists(cache_path):
+                with open(cache_path, "r") as f:
+                    return {"upload_url": f.read(), "cache_path": cache_path}
+            else:
+                return {"upload_url": None, "cache_path": cache_path}
+    return {"upload_url": None, "cache_path": None}
+
+def update_upload_cache(file_path: str, upload_url: str, upload_server: str="aliyun_oss") -> dict:
+    cache = get_upload_cache(file_path, upload_server)
+    cache_file_path = cache['cache_path']
+
+    if cache_file_path and upload_url:
+        os.makedirs(os.path.dirname(cache_file_path), exist_ok=True)
+        with open(cache_file_path, 'w') as f:
+            f.write(upload_url)
+            return {"upload_url": upload_url, "cache_path": cache_file_path}
+
+    return {"upload_url": None, "cache_path": cache_file_path}
+
+def confirm_upload_file(model: str=None, uri: str=None, api_key: str=None):
     try:
         from dashscope.utils.oss_utils import upload_file
     except ImportError:
@@ -20,14 +52,21 @@ def confirm_upload_file(model: str=None, image_path: str=None, api_key: str=None
             "Please install it via 'pip install -U dashscope'"
         )
 
-    if image_path and (image_path.startswith('http://') or image_path.startswith('https://') or image_path.startswith('oss://')):
-        return image_path
-    if os.path.exists(image_path):
-        image = f"file://{os.path.abspath(image_path)}"
-        path = upload_file(model, image, api_key)
-        return path
+    if uri and (uri.startswith('http://') or uri.startswith('https://') or uri.startswith('oss://')):
+        return uri
+    
+    if os.path.exists(uri):
+        abs_path = os.path.abspath(uri)
+        cache = get_upload_cache(abs_path, "aliyun_oss")
+        if cache['cache_path'] and cache['upload_url']:
+            return cache['upload_url']
+        else:
+            oss_file = f"file://{abs_path}"
+            upload_url = upload_file(model, oss_file, api_key)
+            update_upload_cache(abs_path, upload_url, "aliyun_oss")
+            return upload_url
     else:
-        raise ValueError(f"Invalid image path: {image_path}")
+        raise ValueError(f"Invalid URI path: {uri}")
 
 
 def get_headers(api_key: str, enable_async: bool = True) -> Dict[str, str]:
@@ -105,3 +144,5 @@ async def async_check_task_status(status_result: Dict[str, Any], task_id: str, h
                 if _status_result['output']['task_status'] in ['SUCCEEDED', 'FAILED']:
                     status_result.update(_status_result)
                     break
+
+
