6f5d9ccc3e5ec1d69579793d48f5daaf16e9a831
arcstep <43801@qq.com>
2024-10-02 23:48:19 +0800

    feat: 在行动规划的表述语法中增加 <self_solve/>，即自己解决

--- illufly/core/runnable/agent/team/rules/step_by_step.py
+++ illufly/core/runnable/agent/team/rules/step_by_step.py
@@ -28,33 +28,39 @@ class StepByStep(BaseTeam):
         调用团队协作完成任务。
         """
         self._completed_teamwork = []
-        max_rounds = 3
+        max_rounds = 10
+        task_index = 0
         for round in range(max_rounds):
             to_continue = False
             yield EventBlock("agent", f"ROUND {round + 1} @{self.leader.name}")
             yield from self.leader.call(prompt, new_chat=True, **kwargs)
+            self._last_output = self.leader.last_output
+            if not self._completed_teamwork:
+                self._completed_teamwork.append(f'[总任务目标] {self.leader.task}')
+
+            self_solve = self.extract_answer(self.leader.last_output, marker="self_solve")
+            if self_solve:
+                self._completed_teamwork.append(f'[行动] 该子任务自己解决，结果为 {self_solve}')
 
             tasks = self.extract_task_dispatch(self.leader.last_output)
             for task in tasks:
+                task_detail = task["task_detail"]
                 member = self.find_member(task["member_name"])
-                if member:
-                    self._completed_teamwork.append(f'[交办] 一个任务交办给 @{task["member_name"]}, 详细任务为 {task["description"]}')
+                if member and task_detail:
+                    self._completed_teamwork.append(f'[行动] #E{task_index + 1} 子任务交给 @{member.name}, 要求为 {task_detail}')
                     yield EventBlock("agent", f"ROUND {round + 1} @{member.name}")
-                    yield from member.call(task["description"], new_chat=True, **kwargs)
+                    yield from member.call(task_detail, new_chat=True, **kwargs)
 
-                    self._completed_teamwork.append(f'[观察] 由 @{task["member_name"]} 回复， {member.last_output}')
+                    self._completed_teamwork.append(f'[观察] @{member.name} 已完成 #E{task_index + 1} 子任务，回复内容为 {member.last_output}')
                     to_continue = True
-                else:
-                    raise ValueError(f"member {task} not found")
-
-            final_answer = self.extract_final_answer(self.leader.last_output)
-            if final_answer:
-                self._completed_teamwork.append(f'[最终答案] \n{final_answer}')
-                self._last_output = final_answer
-                yield EventBlock("final_answer", final_answer)
-                return final_answer
 
             if not to_continue:
                 break
 
-        yield EventBlock("final_answer", "经过多次尝试，没有找到最终答案")
+        final_answer = self.extract_answer(self.leader.last_output)
+        if final_answer:
+            self._completed_teamwork.append(f'[最终答案] \n{final_answer}')
+            self._last_output = final_answer
+            yield EventBlock("final_answer", final_answer)
+        else:
+            yield EventBlock("final_answer", "经过多次尝试，没有找到最终答案")
