88be1ed83418fb43f41e103316927509b6b0167a
arcstep <43801@qq.com>
2024-10-04 23:17:30 +0800

    feat: 调整 Retriever 的位置

--- illufly/core/runnable/agent/__init__.py
+++ illufly/core/runnable/agent/__init__.py
@@ -2,3 +2,4 @@ from .base import Runnable, BaseAgent
 from .tool import ToolAgent
 from .chat import ChatAgent
 from .team import BaseTeam, StepByStep, Pipe, FromOutline, Discuss
+from .retriever import Retriever
