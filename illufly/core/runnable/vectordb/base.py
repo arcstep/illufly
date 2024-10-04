98407302085d4dc732b83320d9317a1f045578c5
arcstep <43801@qq.com>
2024-10-04 12:17:04 +0800

    feat: FaissDB 加载文本与文件，使用与 MarkMeta 相同方法

--- illufly/core/runnable/vectordb/base.py
+++ illufly/core/runnable/vectordb/base.py
@@ -8,11 +8,20 @@ class VectorDB(Runnable):
     """
 
     def __init__(self, embeddings: BaseEmbeddings, top_k: int=None, **kwargs):
+        if not isinstance(embeddings, BaseEmbeddings):
+            raise ValueError(f"{embeddings} must be an instance of BaseEmbeddings")
+        if embeddings.dim is None:
+            raise ValueError(f"{embeddings} dim must be set")
+
         super().__init__(**kwargs)
-        self.documents = []
+
+        # 必须提供一个向量模型
         self.embeddings = embeddings
-        self.dim = self.embeddings.dim
+        self.dim = embeddings.dim
         self.top_k = top_k or 5
+
+        # 向量数据库中备查的文档，包括了原始文档、元数据，以及由向量模型转换的向量索引
+        self.documents = []
 
     def query(self, vector: List[List[float]], top_k: int=None, **kwargs):
         pass
