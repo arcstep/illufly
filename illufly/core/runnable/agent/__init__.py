3bf201b2cbc8e4bd36278d0e178119c844ec206c
arcstep <43801@qq.com>
2024-10-10 16:54:25 +0800

    add: RouterAgent

--- illufly/core/runnable/agent/__init__.py
+++ illufly/core/runnable/agent/__init__.py
@@ -2,3 +2,4 @@ from .base import Runnable, BaseAgent
 from .chat import ChatAgent, ChatPool
 from .team import BaseTeam, StepByStep, Pipe, FromOutline, Discuss
 from .retriever import Retriever
+from .router import RouterAgent
