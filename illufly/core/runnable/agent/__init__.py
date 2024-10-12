cdf0a49233cdeba27b3fb765766a867280e35bc8
arcstep <43801@qq.com>
2024-10-12 21:13:59 +0800

    feat: 将 RouteAgent 修改为兼容性更好的 Selector

--- illufly/core/runnable/agent/__init__.py
+++ illufly/core/runnable/agent/__init__.py
@@ -2,4 +2,3 @@ from .base import Runnable, BaseAgent
 from .chat import ChatAgent, ChatPool
 from .team import BaseTeam, StepByStep, Pipe, FromOutline, Discuss
 from .retriever import Retriever
-from .router import RouterAgent
