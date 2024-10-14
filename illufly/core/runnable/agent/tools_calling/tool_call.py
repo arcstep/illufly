0ad3f59b8d30cf01b323ccede0b4bc085cdafde6
arcstep <43801@qq.com>
2024-10-14 15:58:51 +0800

    feat: 优化处理 <tool_call/> 标签的逻辑

--- illufly/core/runnable/agent/tools_calling/tool_call.py
+++ illufly/core/runnable/agent/tools_calling/tool_call.py
@@ -6,7 +6,7 @@ import json
 class ToolCall(BaseToolCalling):
 
     def extract_tools_call(self, text: str) -> List[Dict[str, Any]]:
-        tool_calls = []
+        tools_call = []
         start_marker = "<tool_call>"
         end_marker = "</tool_call>"
         start = text.find(start_marker)
@@ -16,7 +16,7 @@ class ToolCall(BaseToolCalling):
                 tool_call_json = text[start + len(start_marker):end]
                 try:
                     tool_call = json.loads(tool_call_json)
-                    tool_calls.append({
+                    tools_call.append({
                         "function": {
                             "name": tool_call.get("name"),
                             "arguments": json.dumps(tool_call.get("arguments", "[]"), ensure_ascii=False)
@@ -27,24 +27,56 @@ class ToolCall(BaseToolCalling):
                 start = text.find(start_marker, end)
             else:
                 break
-        return tool_calls
+        return tools_call
 
-    def handle_tools_call(self, final_tool_call):
-        if self.exec_tool:
-            for block in self.execute_tool(final_tool_call):
-                if isinstance(block, EventBlock) and block.block_type == "tool_resp_final":
-                    tool_resp = block.text
-                    tool_resp_message = [
-                        {
-                            "role": "user",
-                            "content": f'<tool_resp>{tool_resp}</tool_resp>'
-                        }
-                    ]
-                    self.short_term_memory.extend(tool_resp_message)
-                    self.long_term_memory.extend(tool_resp_message)
-                yield block
+    def handle_tools_call(self, text: str):
+        tools_call = self.extract_tools_call(text)
 
-    async def async_handle_tools_call(self, final_tool_call):
+        for index, tool_call in enumerate(tools_call):
+            # 由于工具是从文本中提取的，第一个工具在文本中已经作为AI回复，所以从第二个开始构造记忆
+            if index > 0:
+                new_task = f'请继续: {json.dumps(tool_call, ensure_ascii=False)}'
+                tool_call_message = [
+                    {
+                        "role": "assistant",
+                        "content": new_task
+                    }
+                ]
+                self.short_term_memory.extend(tool_call_message)
+                self.long_term_memory.extend(tool_call_message)
+
+            # 执行工具
+            if self.exec_tool:
+                for block in self.execute_tool(tool_call):
+                    if isinstance(block, EventBlock) and block.block_type == "tool_resp_final":
+                        tool_resp = block.text
+                        tool_resp_message = [
+                            {
+                                "role": "user",
+                                "content": f'<tool_resp>{tool_resp}</tool_resp>'
+                            }
+                        ]
+                        self.short_term_memory.extend(tool_resp_message)
+                        self.long_term_memory.extend(tool_resp_message)
+                    yield block
+
+    async def async_handle_tools_call(self, text: str):
+        tools_call = self.extract_tools_call(text)
+
+        for index, tool_call in enumerate(tools_call):
+            # 由于工具是从文本中提取的，第一个工具在文本中已经作为AI回复，所以从第二个开始构造记忆
+            if index > 0:
+                new_task = f'请继续: {json.dumps(tool_call, ensure_ascii=False)}'
+                tool_call_message = [
+                    {
+                        "role": "assistant",
+                        "content": new_task
+                    }
+                ]
+                self.short_term_memory.extend(tool_call_message)
+                self.long_term_memory.extend(tool_call_message)
+
+        # 执行工具
         if self.exec_tool:
             async for block in self.async_execute_tool(final_tool_call):
                 if isinstance(block, EventBlock) and block.block_type == "tool_resp_final":
