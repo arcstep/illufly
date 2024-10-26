bb62252f8e0931f01a89875a9b7fdd91f4c2fa6e
arcstep <43801@qq.com>
2024-10-26 19:37:35 +0800

    feat: Selector 自动执行首次 select

--- illufly/flow/__init__.py
+++ illufly/flow/__init__.py
@@ -1,2 +1,3 @@
-from ..core.runnable.agent import ChatPool, FlowAgent, ReAct, ReWOO, PlanAndSolve, FromOutline, Learn
+from ..core.runnable.agent import ChatPool, FlowAgent, ReAct, ReWOO, PlanAndSolve, FromOutline
+from ..core.runnable.agent import ChatLearn
 from ..core.runnable import Selector, End
