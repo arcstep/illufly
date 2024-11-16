5c6f9b447b30063a5160bf1c4bb790af04a23195
arcstep <43801@qq.com>
2024-11-16 11:39:10 +0800

    feat: 优化 auth 模块的文档

--- illufly/auth/auth.py
+++ illufly/auth/auth.py
@@ -18,38 +18,63 @@ from ..config import get_env
 SECRET_KEY = get_env("FASTAPI_SECRET_KEY")
 ALGORITHM = get_env("FASTAPI_ALGORITHM")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+# 定义tokenUrl
+TOKEN_URL = "login"
+
+# 初始化OAuth2PasswordBearer
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=TOKEN_URL)
 
 TOKEN_BLACKLIST = set()
 
 
-# 修改refresh_token和logout逻辑，以及在生成新的刷新令牌时调用add_refresh_token_to_whitelist
-# 在refresh_token逻辑中首先调用is_refresh_token_in_whitelist检查令牌是否有效
-# 在logout逻辑中调用remove_refresh_token_from_whitelist移除令牌
+# 修改 refresh_token 和 logout 逻辑，并在生成新的刷新令牌时调用 add_refresh_token_to_whitelist
+# 在 refresh_token 逻辑中首先调用 is_refresh_token_in_whitelist 检查令牌是否有效
+# 在 logout 逻辑中调用 remove_refresh_token_from_whitelist 移除令牌
 
 def default_auth_func(username: str, password: str):
-    """默认的用户认证方法，需要自定义并传入给 `create_auth_api`"""
+    """
+    默认的用户认证方法，需要自定义并传入给 `create_auth_api`
+
+    例子：
+
+    ```
+    from fastapi import FastAPI
+    from illufly.auth import create_auth_api
+
+    def custom_auth_func(username: str, password: str):
+        # 这里可以添加自定义的认证逻辑
+        return {"username": username}
+
+    app = FastAPI()
+    app.include_router(create_auth_api(custom_auth_func))
+    ```
+    """
     return {"username": username}
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     """从JWT中解析当前用户"""
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
+        detail=f"Could not validate credentials. Please obtain a valid token from {TOKEN_URL}",
         headers={"WWW-Authenticate": "Bearer"},
     )
 
     try:
-        # 使用verify_jwt方法验证JWT并获取payload
-        if is_access_token_in_whitelist(token):
-            username: str = verify_jwt(token)
-            if username is None:
-                raise credentials_exception
-        else:
+        # 检查令牌是否在白名单中
+        if not is_access_token_in_whitelist(token):
             raise credentials_exception
+
+        # 使用 verify_jwt 方法验证 JWT 并获取 payload
+        username: str = verify_jwt(token)
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        # 捕获JWT解码错误并抛出自定义异常
+        raise credentials_exception
     except HTTPException as e:
-        # 直接抛出verify_jwt中可能产生的异常
+        # 直接抛出 verify_jwt 中可能产生的异常
         raise e
+
     return {"username": username}
 
 def create_auth_api(auth_func: callable=None):
@@ -111,7 +136,7 @@ def create_auth_api(auth_func: callable=None):
 
 def verify_jwt(token: str):
     if token.lower().startswith("bearer "):
-        token = token[7:]  # 去除"Bearer "前缀
+        token = token[7:]  # 去除 "Bearer " 前缀
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
