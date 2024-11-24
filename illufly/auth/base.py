11fb4e8c2d3b04d982976a8e3f4fe35fa1ea272f
arcstep <43801@qq.com>
2024-11-24 20:43:00 +0800

    feat: 改进 get_current_user

--- illufly/auth/base.py
+++ illufly/auth/base.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, APIRouter, Depends, HTTPException, status, Request, Response
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
 from typing import Dict
@@ -18,18 +17,7 @@ from ..config import get_env
 SECRET_KEY = get_env("FASTAPI_SECRET_KEY")
 ALGORITHM = get_env("FASTAPI_ALGORITHM")
 
-# 定义tokenUrl
-TOKEN_URL = "login"
-
-# 初始化OAuth2PasswordBearer
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=TOKEN_URL)
-
 TOKEN_BLACKLIST = set()
-
-
-# 修改 refresh_token 和 logout 逻辑，并在生成新的刷新令牌时调用 add_refresh_token_to_whitelist
-# 在 refresh_token 逻辑中首先调用 is_refresh_token_in_whitelist 检查令牌是否有效
-# 在 logout 逻辑中调用 remove_refresh_token_from_whitelist 移除令牌
 
 def default_auth_func(username: str, password: str):
     """
@@ -56,32 +44,26 @@ async def get_current_user(request: Request):
     if not token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Not authenticated",
-            headers={"WWW-Authenticate": "Bearer"},
+            detail="Not authenticated"
         )
 
     try:
-        # 检查令牌是否在白名单中
         if not is_access_token_in_whitelist(token):
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Token not in whitelist",
-                headers={"WWW-Authenticate": "Bearer"},
+                detail="Token not in whitelist"
             )
 
-        # 使用 verify_jwt 方法验证 JWT 并获取 payload
         username: str = verify_jwt(token)
         if username is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid token",
-                headers={"WWW-Authenticate": "Bearer"},
+                detail="Invalid token"
             )
     except JWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
+            detail="Could not validate credentials"
         )
 
     return {"username": username}
@@ -102,7 +84,6 @@ def create_auth_api(auth_func: callable=None):
 
     @router.post("/token/refresh")
     async def refresh_token(request: Request):
-        # 从 Cookies 中获取 refresh_token
         refresh_token = request.cookies.get("refresh_token")
         if not refresh_token or not is_refresh_token_in_whitelist(refresh_token):
             raise HTTPException(status_code=403, detail="Refresh token is not in whitelist")
@@ -113,17 +94,16 @@ def create_auth_api(auth_func: callable=None):
                 raise HTTPException(status_code=400, detail="Invalid refresh token")
             new_access_token = _create_access_token(data={"sub": username})
             
-            # 设置新的 access_token 到 HttpOnly Cookie
             response = Response()
             response.set_cookie(
                 key="access_token",
                 value=new_access_token,
                 httponly=True,
-                secure=True,  # 在生产环境中使用 HTTPS 时设置为 True
-                samesite="Lax"  # 或者 "Strict" 根据需求
+                secure=True,
+                samesite="Lax"
             )
             
-            return {"access_token": new_access_token, "token_type": "bearer"}
+            return {"access_token": new_access_token}
         except JWTError:
             raise HTTPException(status_code=403, detail="Token is expired or invalid")
 
@@ -138,7 +118,6 @@ def create_auth_api(auth_func: callable=None):
         access_token = _create_access_token(data=user_info)
         refresh_token = _create_refresh_token(data=user_info)
 
-        # 设置 HttpOnly Cookie
         set_auth_cookies(response, access_token, refresh_token)
 
         return user_info
@@ -148,7 +127,6 @@ def create_auth_api(auth_func: callable=None):
         remove_access_token_from_whitelist(current_user)
         remove_refresh_token_from_whitelist(current_user)
         
-        # 清除 cookies
         response.delete_cookie(key="access_token")
         response.delete_cookie(key="refresh_token")
         
@@ -162,17 +140,13 @@ def create_auth_api(auth_func: callable=None):
     return router
 
 def verify_jwt(token: str):
-    if token.lower().startswith("bearer "):
-        token = token[7:]  # 去除 "Bearer " 前缀
-
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload.get("username")
     except JWTError as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
+            detail="Could not validate credentials"
         ) from e
 
 def _create_refresh_token(data: dict):
@@ -198,14 +172,14 @@ def set_auth_cookies(response: Response, access_token: str, refresh_token: str):
         key="refresh_token",
         value=refresh_token,
         httponly=True,
-        secure=True,  # 在生产环境中使用 HTTPS 时设置为 True
-        samesite="Lax"  # 或者 "Strict" 根据需求
+        secure=True,
+        samesite="Lax"
     )
     response.set_cookie(
         key="access_token",
         value=access_token,
         httponly=True,
-        secure=True,  # 在生产环境中使用 HTTPS 时设置为 True
-        samesite="Lax"  # 或者 "Strict" 根据需求
+        secure=True,
+        samesite="Lax"
     )
 
