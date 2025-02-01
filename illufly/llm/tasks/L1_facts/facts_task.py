17f1df686bbfbf18914cdd3963ab35d6a8ef83a6
arcstep <43801@qq.com>
2025-02-02 00:53:12 +0800

    add: 增加事实管理

--- illufly/llm/tasks/L1_facts/facts_task.py
+++ illufly/llm/tasks/L1_facts/facts_task.py
@@ -10,12 +10,13 @@ from ....envir import get_env
 from ....io.rocksdict import IndexedRocksDB
 from ...system_template import SystemTemplate
 from ...memory.L0_qa import QA, Message
+from ...memory.L1_facts import Fact
 from ...chat_openai import ChatOpenAI
 from ...memory.types import TaskState, MemoryType
 from ..base_task import BaseTask
 
-class QaTask(BaseTask):
-    """对话摘要任务"""
+class FactsTask(BaseTask):
+    """提取事实任务"""
     
     @classmethod
     def get_error_tasks(cls, db: IndexedRocksDB, limit: int = None):
@@ -23,7 +24,7 @@ class QaTask(BaseTask):
         limit = limit or 1000
         values = db.values_with_indexes(
             model_name=MemoryType.QA,
-            field_path="task_summarize",
+            field_path="task_extract_facts",
             field_value=TaskState.ERROR,
             limit=limit
         )
@@ -35,12 +36,11 @@ class QaTask(BaseTask):
         limit = limit or 1000
         values = db.values_with_indexes(
             model_name=MemoryType.QA,
-            field_path="task_summarize",
+            field_path="task_extract_facts",
             field_value=TaskState.PROCESSING,
             limit=limit
         )
         return [QA(**v) for v in values] if values else []
-
 
     @classmethod
     def get_todo_tasks(cls, db: IndexedRocksDB, limit: int = None):
@@ -48,7 +48,7 @@ class QaTask(BaseTask):
         limit = limit or 1000
         values = db.values_with_indexes(
             model_name=MemoryType.QA,
-            field_path="task_summarize",
+            field_path="task_extract_facts",
             field_value=TaskState.TODO,
             limit=limit
         )
@@ -59,36 +59,37 @@ class QaTask(BaseTask):
         """获取待处理的QA列表"""
         values = db.values_with_indexes(
             model_name=MemoryType.QA,
-            field_path="task_summarize",
+            field_path="task_extract_facts",
             field_value=TaskState.TODO,
             limit=1
         )
         qa = QA(**values[0]) if values else None
         if qa:
-            qa.task_summarize = TaskState.PROCESSING
+            qa.task_extract_facts = TaskState.PROCESSING
             db.update_with_indexes(MemoryType.QA, qa.key, qa.model_dump())
             updated_qa = QA(**db[qa.key])
-            logging.debug(f"获取QA {qa.qa_id} 并设置为处理中: {updated_qa.task_summarize}")
+            logging.debug(f"获取QA {qa.qa_id} 并设置为处理中: {updated_qa.task_extract_facts}")
         return qa
 
     @classmethod
-    async def _process_summary(
+    async def _generate_facts(
         cls,
         db: IndexedRocksDB,
         messages: List[Dict[str, Any]],
-        content: str,
+        question: str,
+        answer: str,
         logger: logging.Logger,
         assistant: ChatOpenAI
     ):
-        """处理摘要"""
+        """生成事实"""
         # logger.debug(f"开始处理摘要任务， memory: {messages}, content: {content}")
         chat = assistant
 
-        template = SystemTemplate(template_id="summary")
+        template = SystemTemplate(template_id="facts")
         resp = chat.async_call(
-            messages="请开始处理",
+            messages="请开始生成事实",
             system_template=template,
-            bindings={"memory": messages, "content": content}
+            bindings={"memory": messages, "question": question, "answer": answer}
         )
 
         final_text = ""
@@ -97,7 +98,15 @@ class QaTask(BaseTask):
             if chunk.block_type == BlockType.TEXT_CHUNK:
                 final_text += chunk.text
 
-        return final_text if final_text else content
+        if len(final_text) > 10:
+            fact = Fact(
+                user_id=qa.user_id,
+                thread_id=qa.thread_id,
+                title=final_text[:30],
+                content=final_text,
+                source_chat_threads=[qa.qa_id]
+            )
+            db.update_with_indexes(MemoryType.FACT, fact.key, fact.model_dump())
 
     @classmethod
     def reset_processing_task(cls, db: IndexedRocksDB, batch_size: int = None):
@@ -106,7 +115,7 @@ class QaTask(BaseTask):
         while True:
             items = db.values_with_indexes(
                 model_name=MemoryType.QA,
-                field_path="task_summarize",
+                field_path="task_extract_facts",
                 field_value=TaskState.PROCESSING,
                 limit=batch_size
             )
@@ -115,7 +124,7 @@ class QaTask(BaseTask):
 
             for item in items:
                 qa = QA(**item)
-                qa.task_summarize = TaskState.TODO
+                qa.task_extract_facts = TaskState.TODO
                 db.update_with_indexes(MemoryType.QA, qa.key, qa.model_dump())
 
     # 以下是 BaseTask 要求的抽象方法实现
@@ -125,7 +134,7 @@ class QaTask(BaseTask):
         """获取一个待处理的任务，如果没有则返回None"""
         values = db.values_with_indexes(
             model_name=MemoryType.QA,
-            field_path="task_summarize",
+            field_path="task_extract_facts",
             field_value=TaskState.TODO,
             limit=1
         )
@@ -135,17 +144,17 @@ class QaTask(BaseTask):
     async def task_to_processing(cls, db: IndexedRocksDB, task: Any) -> None:
         """将任务状态更新为处理中"""
         if task and isinstance(task, QA):
-            task.task_summarize = TaskState.PROCESSING
+            task.task_extract_facts = TaskState.PROCESSING
             db.update_with_indexes(MemoryType.QA, task.key, task.model_dump())
             updated_task = QA(**db[task.key])
-            logging.debug(f"获取QA {task.qa_id} 并设置为处理中: {updated_task.task_summarize}")
+            logging.debug(f"获取QA {task.qa_id} 并设置为处理中: {updated_task.task_extract_facts}")
         else:
             logging.warning(f"任务不是QA类型: {task}")
 
     @classmethod
     async def process_todo_task(cls, db, task: Any, assistant: ChatOpenAI, **kwargs):
-        """处理一个摘要任务"""
-        if not isinstance(task, QA) or not task.task_summarize == TaskState.PROCESSING:
+        """处理一个事实生成任务"""
+        if not isinstance(task, QA) or not task.task_extract_facts == TaskState.PROCESSING:
             raise ValueError(f"任务不是待处理状态的QA类型: {task}")
         
         task_id = cls.get_task_id()
@@ -153,32 +162,12 @@ class QaTask(BaseTask):
         # logger.debug(f"开始处理QA {task.qa_id}")
 
         try:
-            if len(task.question) > 50:
-                # logger.debug(f"开始处理QA {task.qa_id} 的问题摘要")
-                messages = [m.message_dict for m in task.messages]
-                summary_question = await cls._process_summary(db, messages, task.question, logger, assistant)
-            else:
-                summary_question = task.question
-
-            if len(task.answer) > 50:
-                # logger.debug(f"开始处理QA {task.qa_id} 的回答摘要")
-                messages = [m.message_dict for m in task.messages]
-                summary_answer = await cls._process_summary(db, messages, task.answer, logger, assistant)
-            else:
-                summary_answer = task.answer
-
-            # logger.debug(f"处理QA {task.qa_id} 完成: {summary_question}, {summary_answer}")
-
-            task.summary = [
-                Message(role="user", content=str(summary_question)),
-                Message(role="assistant", content=str(summary_answer))
-            ]
-            
-            task.task_summarize = TaskState.DONE
-            # logger.debug(f"完成QA {task.qa_id} 的摘要处理")
+            messages = [m.message_dict for m in task.messages]
+            await cls._generate_facts(db, messages, task.question, task.answer, logger, assistant)
+            task.task_extract_facts = TaskState.DONE
             db.update_with_indexes(MemoryType.QA, task.key, task.model_dump())
 
         except Exception as e:
             logger.error(f"处理QA {task.qa_id} 时发生错误: {e}")
-            task.task_summarize = TaskState.ERROR
+            task.task_extract_facts = TaskState.ERROR
             db.update_with_indexes(MemoryType.QA, task.key, task.model_dump())
